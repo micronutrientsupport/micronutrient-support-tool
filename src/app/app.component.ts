4c8131f642b36a5115a8ea3c239dbb21b3c65424
Steve Richardson <stever@bgs.ac.uk>
2020-11-16 12:21:30 +0000

    ci: testing if error in linting is picked up by action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'micronutrient-support-tool';
 }
+
+// comment with treally long long long line to test linting in action error comment with treally long long long line to test linting in action error comment with treally long long long line to test linting in action error
