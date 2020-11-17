8c4a3592f032e65767a32f4750271b469295200f
Steve Richardson <stever@bgs.ac.uk>
2020-11-17 16:23:03 +0000

    style: added material import module for use in components and pages

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,22 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AppMaterialModule } from '../app-material.module';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
-
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-  ],
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, AppMaterialModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AppMaterialModule } from '../app-material.module';
 import { HomeComponent } from './home/home.component';
 import { MapsToolComponent } from './mapsTool/mapsTool.component';
 import { QuickMapsComponent } from './quickMaps/quickMaps.component';
 
 @NgModule({
   declarations: [HomeComponent, MapsToolComponent, QuickMapsComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, AppMaterialModule],
   providers: [],
   exports: [HomeComponent, MapsToolComponent, QuickMapsComponent],
 })
