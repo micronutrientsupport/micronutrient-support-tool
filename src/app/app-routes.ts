a933e79eec3ca1a1a98513f89205e68c30bb3790
jonstu <jonstu@bgs.ac.uk>
2020-11-17 11:07:04 +0000

    Try fix linting

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -5,31 +5,37 @@ export interface AppRoute {
 
 /**
  * TODO: This would be more efficient as a pipe?
+ *
  * @param appRoute an AppRoute object
  * @param substitutions key-value pairs to replace placeholders in route e.g. {job-id: 54}
  * will change a route of "job/:job-id/details" to "job/54/details"
  */
-function getRoute(appRoute: AppRoute, substitutions = {}): Array<string> {
+const getRoute = (appRoute: AppRoute, substitutions = {}): Array<string> => {
   let url = `/${appRoute.key}`;
   Object.keys(substitutions).forEach((key: string) => {
     url = url.replace(`:${key}`, substitutions[key]);
   });
 
   return [url];
-}
+};
 
 export class AppRoutes {
   public static readonly HOME = {
     key: '',
-    route(): Array<string> { return getRoute(this); }
+    route(): Array<string> {
+      return getRoute(this);
+    },
   } as AppRoute;
   public static readonly MAPS_TOOL = {
     key: 'maps-tool',
-    route(): Array<string> { return getRoute(this); }
+    route(): Array<string> {
+      return getRoute(this);
+    },
   } as AppRoute;
   public static readonly QUICK_MAPS = {
     key: `${AppRoutes.MAPS_TOOL.key}/quick-maps`,
-    route(): Array<string> { return getRoute(this); }
+    route(): Array<string> {
+      return getRoute(this);
+    },
   } as AppRoute;
 }
-
