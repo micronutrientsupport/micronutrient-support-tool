72225fc51c0836dcbb6a9a7a0974a3d64f661ed3
jonstu <jonstu@bgs.ac.uk>
2020-11-17 10:08:18 +0000

    Try fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,6 @@ module.exports = {
       },
     ],
   ],
-  '@angular-eslint/no-conflicting-lifecycle': 'error',
   '@angular-eslint/no-host-metadata-property': 'error',
   '@angular-eslint/no-input-rename': 'error',
   '@angular-eslint/no-inputs-metadata-property': 'error',
