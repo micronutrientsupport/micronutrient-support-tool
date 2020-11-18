28387adb290ca0934675e62420ebc6d834315d60
Warren <danwar@bgs.ac.uk>
2020-11-18 11:15:03 +0000

    feat: leaflet installed and map displaying in map-tools component

--- src/app/pages/mapsTool/mapsTool.component.ts
+++ src/app/pages/mapsTool/mapsTool.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 import { AppRoutes } from 'src/app/app-routes';
 
 @Component({
@@ -6,6 +7,13 @@ import { AppRoutes } from 'src/app/app-routes';
   templateUrl: './mapsTool.component.html',
   styleUrls: ['./mapsTool.component.scss'],
 })
-export class MapsToolComponent {
+export class MapsToolComponent implements OnInit {
   public ROUTES = AppRoutes;
+
+  ngOnInit(): void {
+    const myMap = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppMaterialModule } from '../app-material.module';
 import { HomeComponent } from './home/home.component';
 import { MapsToolComponent } from './mapsTool/mapsTool.component';
@@ -8,8 +9,8 @@ import { QuickMapsComponent } from './quickMaps/quickMaps.component';
 
 @NgModule({
   declarations: [HomeComponent, MapsToolComponent, QuickMapsComponent],
-  imports: [CommonModule, RouterModule, AppMaterialModule],
+  imports: [CommonModule, RouterModule, AppMaterialModule, LeafletModule],
   providers: [],
   exports: [HomeComponent, MapsToolComponent, QuickMapsComponent],
 })
-export class PagesModule {}
+export class PagesModule { }
