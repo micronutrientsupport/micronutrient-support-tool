a1d15f0684447350133b77ebd1877ab480cea26d
joda <joda@bgs.ac.uk>
2020-11-18 10:26:33 +0000

    chore: removed the font awesome svg import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagesModule } from './pages/pages.module';
 import { ComponentsModule } from './components/components.module';
-import {  } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [AppComponent],
