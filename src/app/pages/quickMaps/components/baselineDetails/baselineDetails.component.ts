651d4a7f7f2ffacdb7b0e52cdcbcb8b1cb7f276d
Warren <danwar@bgs.ac.uk>
2020-11-19 15:23:37 +0000

    feat: added button toggle to quickmap viewer

---
+++ src/app/pages/quickMaps/components/baselineDetails/baselineDetails.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-baseline-details',
+  templateUrl: './baselineDetails.component.html',
+  styleUrls: ['./baselineDetails.component.scss'],
+})
+export class BaselineDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/pages/quickMaps/quickMaps.module.ts
+++ src/app/pages/quickMaps/quickMaps.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { HttpClientModule } from '@angular/common/http';
 import { AppMaterialModule } from 'src/app/app-material.module';
-
-import { QuickMapsComponent } from './pages/quickMaps.component';
-import { MapPageComponent } from './pages/components/mapPage/mapPage.component';
-import { SideNavContentComponent } from './pages/components/sideNavContent/sideNavContent.component';
+import { QuickMapsComponent } from './quickMaps.component';
+import { MapPageComponent } from './components/mapPage/mapPage.component';
+import { SideNavContentComponent } from './components/sideNavContent/sideNavContent.component';
+import { BaselineDetailsComponent } from './components/baselineDetails/baselineDetails.component';
 
 @NgModule({
-  declarations: [QuickMapsComponent, MapPageComponent, SideNavContentComponent],
+  declarations: [QuickMapsComponent, MapPageComponent, SideNavContentComponent, BaselineDetailsComponent],
   imports: [CommonModule, LeafletModule, HttpClientModule, AppMaterialModule],
 })
 export class QuickMapsModule {}
