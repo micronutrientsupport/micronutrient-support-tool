ff42efe9c00d6764625e3977bd1f44f4abdce7e0
Warren <danwar@bgs.ac.uk>
2020-11-19 13:05:26 +0000

    refactor: component restructure

---
+++ src/app/pages/quickMaps/pages/components/sideNavContent/sideNavContent.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav-content',
+  templateUrl: './sideNavContent.component.html',
+  styleUrls: ['./sideNavContent.component.scss'],
+})
+export class SideNavContentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
