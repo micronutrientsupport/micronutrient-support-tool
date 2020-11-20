56cfa01d65b2c5638a5f328f87186cffe53fdc05
joda <joda@bgs.ac.uk>
2020-11-20 11:33:03 +0000

    feat: added form scaffolding into the side nav and moved dictionary calls from quickMaps.component into sideNavContent.component

--- src/app/pages/quickMaps/quickMaps.module.ts
+++ src/app/pages/quickMaps/quickMaps.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { HttpClientModule } from '@angular/common/http';
 import { AppMaterialModule } from 'src/app/app-material.module';
-
 import { QuickMapsComponent } from './pages/quickMaps.component';
 import { MapPageComponent } from './pages/components/mapPage/mapPage.component';
 import { SideNavContentComponent } from './pages/components/sideNavContent/sideNavContent.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [QuickMapsComponent, MapPageComponent, SideNavContentComponent],
-  imports: [CommonModule, LeafletModule, HttpClientModule, AppMaterialModule],
+  imports: [CommonModule, LeafletModule, HttpClientModule, AppMaterialModule, ReactiveFormsModule],
 })
 export class QuickMapsModule {}
