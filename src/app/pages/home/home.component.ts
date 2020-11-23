1f3e2a0c16adb0335dc2ac87fb0e55353f4f180f
Steve Richardson <stever@bgs.ac.uk>
2020-11-23 08:26:50 +0000

    fix: removed pages from exports as causing compile errors

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AppRoutes } from 'src/app/routes/routes';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent { }
+export class HomeComponent {
+  public ROUTES = AppRoutes;
+}
