293be139c3611b7fbb05557d5e5065b63b3f8e24
Warren <danwar@bgs.ac.uk>
2020-11-24 14:39:34 +0000

    chore: remove typescript linting and dead code

--- src/app/pages/quickMaps/pages/baselineDetails/baselineDetails.component.ts
+++ src/app/pages/quickMaps/pages/baselineDetails/baselineDetails.component.ts
@@ -1,9 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { HttpClient } from '@angular/common/http';
-import { Papa } from 'ngx-papaparse';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,6 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./baselineDetails.component.scss'],
 })
 export class BaselineDetailsComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void { }
+  constructor() {}
+  ngOnInit(): void {}
 }
