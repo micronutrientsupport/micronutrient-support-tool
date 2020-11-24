24601b7d482954b3850da6a5274da11302b2b2d9
jonstu <jonstu@bgs.ac.uk>
2020-11-24 10:15:59 +0000

    feat: continue to add quick maps route guard checks query params Update params when selection set to quickMaps.service.

--- src/app/pages/quickMaps/quickMaps.service.ts
+++ src/app/pages/quickMaps/quickMaps.service.ts
@@ -1,25 +1,56 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { QuickMapsQueryParams } from './quickMapsQueryParams';
 
 @Injectable()
 export class QuickMapsService {
-  private static readonly QUERY_PARAM_COUNTRY_ID = 'countryId';
-  private readonly selectedCountryIdSrc = new BehaviorSubject<string>(null);
+  private readonly countryIdSrc = new BehaviorSubject<string>(null);
   // eslint-disable-next-line @typescript-eslint/member-ordering
-  public selectedCountryObs = this.selectedCountryIdSrc.asObservable();
+  public countryIdObs = this.countryIdSrc.asObservable();
 
-  constructor(private activatedRoute: ActivatedRouteSnapshot) {
-    this.setSelectedCountryId(this.activatedRoute.queryParamMap.get(QuickMapsService.QUERY_PARAM_COUNTRY_ID));
+  private readonly micronutrientIdsSrc = new BehaviorSubject<Array<string>>(null);
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public micronutrientIdsObs = this.micronutrientIdsSrc.asObservable();
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    route: ActivatedRoute,
+  ) {
+    // set from query params on init
+    this.setCountryId(QuickMapsQueryParams.getCountryId(route.snapshot));
+    this.setMicronutrientIds(QuickMapsQueryParams.getMicronutrientIds(route.snapshot));
+
+    this.countryIdObs.subscribe(() => this.updateQueryParams());
+    this.micronutrientIdsObs.subscribe(() => this.updateQueryParams());
   }
 
   public get selectedCountryId(): string {
-    return this.selectedCountryIdSrc.value;
+    return this.countryIdSrc.value;
   }
-
-  public setSelectedCountryId(countryId: string, force = false): void {
+  public setCountryId(countryId: string, force = false): void {
     if (force || (this.selectedCountryId !== countryId)) {
-      this.selectedCountryIdSrc.next(countryId);
+      this.countryIdSrc.next(countryId);
     }
   }
+
+  public get selectedMicronutrientIds(): Array<string> {
+    return this.micronutrientIdsSrc.value;
+  }
+  public setMicronutrientIds(micronutrientIds: Array<string>, force = false): void {
+    micronutrientIds = QuickMapsQueryParams.filterAndSortArray(micronutrientIds);
+    if (force || (QuickMapsQueryParams.compareArrays(micronutrientIds, this.selectedMicronutrientIds))) {
+      this.micronutrientIdsSrc.next(micronutrientIds);
+    }
+  }
+
+  private updateQueryParams(): void {
+    const paramsObj = {} as Record<string, string | Array<string>>;
+    paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.COUNTRY_ID] = this.selectedCountryId;
+    paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.MICRONUTRIENT_IDS] = this.selectedMicronutrientIds;
+    QuickMapsQueryParams.setQueryParams(this.router, this.activatedRoute, paramsObj);
+
+  }
+
 }
