4c68f686cb7c01a2b6c2e9653d601f9878929880
Steve Richardson <stever@bgs.ac.uk>
2020-11-24 14:40:57 +0000

    feat: created service for sharing to reduce code duplication over multiple pages

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { NgNavigatorShareService } from 'ng-navigator-share';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogShareComponent } from 'src/app/components/dialog/share/dialogShare.component';
 import { Router } from '@angular/router';
+import { SharingService } from 'src/app/services/sharing.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,30 +12,9 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
   public ROUTES = AppRoutes;
-  constructor(
-    private ngNavigatorShareService: NgNavigatorShareService,
-    private dialog: MatDialog,
-    private router: Router,
-  ) {}
+  constructor(private sharingService: SharingService) {}
 
   public share(text: string, title?: string, url?: string): void {
-    if (!this.ngNavigatorShareService.canShare()) {
-      console.log('This service/api is not supported in your Browser');
-      this.dialog.open(DialogShareComponent, { data: { shareLink: window.location.href } });
-      return;
-    }
-
-    this.ngNavigatorShareService
-      .share({
-        title,
-        text,
-        url,
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    this.sharingService.share(text, title, url);
   }
 }
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -4,11 +4,12 @@ import { DictionaryService } from './dictionary.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CurrentDataService } from './currentData.service';
 import { SnackbarService } from './snackbar.service';
+import { SharingService } from './sharing.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
   exports: [],
-  providers: [DictionaryService, CurrentDataService, SnackbarService],
+  providers: [DictionaryService, CurrentDataService, SnackbarService, SharingService],
 })
 export class ServicesModule {}
