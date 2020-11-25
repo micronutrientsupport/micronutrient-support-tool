913445bd4d4c5997cc729dd8b414c694d5dec290
jonstu <jonstu@bgs.ac.uk>
2020-11-25 09:14:53 +0000

    build: fix linting

--- src/app/apiAndObjects/api/currentData/getMicroNutrientDataOptions.ts
+++ src/app/apiAndObjects/api/currentData/getMicroNutrientDataOptions.ts
@@ -3,10 +3,10 @@ import { MicronutrientDataOption } from '../../objects/micronutrientDataOption';
 import { Endpoint } from '../../_lib_code/api/endpoint.abstract';
 
 export class GetMicronutrientDataOptions extends Endpoint<
-  Array<MicronutrientDataOption>,
-  GetMicronutrientDataOptionsParams,
-  MicronutrientDataOption
-  > {
+Array<MicronutrientDataOption>,
+GetMicronutrientDataOptionsParams,
+MicronutrientDataOption
+> {
   protected callLive(): Promise<Array<MicronutrientDataOption>> {
     throw new Error('Method not implemented.');
     // const callResponsePromise = this.apiCaller.doCall('', RequestMethod.GET, {
--- src/app/pages/quickMaps/components/sideNavContent/geographyTypes.enum.ts
+++ src/app/pages/quickMaps/components/sideNavContent/geographyTypes.enum.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-shadow
 export enum GeographyTypes {
   COUNTRY = 'nation',
   REGION = 'area',
