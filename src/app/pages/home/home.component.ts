d8c962dbc0b055558ee5fc301fa946e28a96fc07
Steve Richardson <stever@bgs.ac.uk>
2020-11-25 08:28:40 +0000

    fix: refactor share dialog into combined service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { AppRoutes } from 'src/app/routes/routes';
-import { NgNavigatorShareService } from 'ng-navigator-share';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogShareComponent } from 'src/app/components/dialog/share/dialogShare.component';
-import { Router } from '@angular/router';
 import { SharingService } from 'src/app/services/sharing.service';
 @Component({
   selector: 'app-home',
--- src/app/services/sharing.service.ts
+++ src/app/services/sharing.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NgNavigatorShareService } from 'ng-navigator-share';
-import { DialogShareComponent } from '../components/dialog/share/dialogShare.component';
+import { DialogService } from '../components/dialogs/dialog.service';
 
 @Injectable()
 export class SharingService {
-  constructor(private ngNavigatorShareService: NgNavigatorShareService, private dialog: MatDialog) {}
+  constructor(
+    private ngNavigatorShareService: NgNavigatorShareService,
+    private dialog: MatDialog,
+    private modalService: DialogService,
+  ) {}
 
   public share(text: string, title?: string, url?: string): void {
     if (!this.ngNavigatorShareService.canShare()) {
       console.log('This service/api is not supported in your Browser');
-      this.dialog.open(DialogShareComponent, { id: 'sharingDialog', data: { shareLink: window.location.href } });
+      this.modalService.openShareDialog(window.location.href);
+      // this.dialog.open(DialogShareComponent, { id: 'sharingDialog', data: { shareLink: window.location.href } });
       return;
     }
 
