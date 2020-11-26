7f196b6189ba1bc7633397c210cec4e02f857f30
Javidyousaf <jyou@bgs.ac.uk>
2020-11-26 14:20:24 +0000

    chore: initial commit

--- src/app/pages/quickMaps/pages/baselineDetails/food-composition-view/food-composition-view.component.ts
+++ src/app/pages/quickMaps/pages/baselineDetails/food-composition-view/food-composition-view.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { DialogService } from 'src/app/components/dialogs/dialog.service';
 
 @Component({
   selector: 'app-food-composition-view',
   templateUrl: './food-composition-view.component.html',
-  styleUrls: ['./food-composition-view.component.scss']
+  styleUrls: ['./food-composition-view.component.scss'],
 })
 export class FoodCompositionViewComponent implements OnInit {
+  private content: any;
 
-  constructor() { }
+  constructor(private modalService: DialogService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public tabChanged(tabChangeEvent: MatTabChangeEvent): void {
+    if (tabChangeEvent.index === 1) {
+      // food composition tab
+    } else if (tabChangeEvent.index === 2) {
+      // Threshold tab
+    } else {
+      // index 0
+    }
   }
 
+  public openDialog(): void {
+    // get the active tab content and push to the modal;
+    void this.modalService.openChart('Food composition modal');
+  }
 }
