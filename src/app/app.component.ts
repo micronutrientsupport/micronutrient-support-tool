584d14e4fe5e3e445c8fa72da66803e2df245cdd
jonstu <jonstu@bgs.ac.uk>
2021-01-19 10:43:22 +0000

    chore: remove space

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
     // on navigation, guard checking can take a little time, so show page loading indicator
     router.events.subscribe(event => {
       if (event instanceof GuardsCheckStart) {
-        this. pageLoadingService.showLoading(true);
+        this.pageLoadingService.showLoading(true);
         // console.log('GuardStart');
       }
       if (event instanceof GuardsCheckEnd) {
