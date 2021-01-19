e0b828c3c14dd5773fef027b5f39571bd0cabe05
jonstu <jonstu@bgs.ac.uk>
2021-01-19 09:08:13 +0000

    feat: show the page loading component when routeguards are checking access (currently on quickmaps)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { GuardsCheckEnd, GuardsCheckStart, Router } from '@angular/router';
+import { PageLoadingService } from './services/pageLoadingService.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'micronutrient-support-tool';
 
-  constructor() {}
+  constructor(
+    router: Router,
+    public pageLoadingService: PageLoadingService,
+  ) {
+    // on navigation, guard checking can take a little time, so show page loading indicator
+    router.events.subscribe(event => {
+      if (event instanceof GuardsCheckStart) {
+        this. pageLoadingService.showLoading(true);
+        // console.log('GuardStart');
+      }
+      if (event instanceof GuardsCheckEnd) {
+        this.pageLoadingService.showLoading(false);
+        // console.log('GuardEnd');
+      }
+    });
+  }
+
+
 }
