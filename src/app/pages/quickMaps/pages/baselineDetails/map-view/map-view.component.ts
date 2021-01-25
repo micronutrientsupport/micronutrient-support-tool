cccda9f4c6cc744dc5c77904bfbde70055feb75d
Svea <svea@bgs.ac.uk>
2021-01-25 16:59:37 +0000

    refactor: chloropleth mapp added and sample sub regions refactored to suit

--- src/app/pages/quickMaps/pages/baselineDetails/map-view/map-view.component.ts
+++ src/app/pages/quickMaps/pages/baselineDetails/map-view/map-view.component.ts
@@ -23,7 +23,7 @@ export class MapViewComponent implements AfterViewInit {
   private mapView1: L.Map;
   private mapView2: L.Map;
 
-  constructor(private http: HttpClient, private modalService: DialogService) { }
+  constructor(private http: HttpClient, private modalService: DialogService) {}
 
   ngAfterViewInit(): void {
     // fails to find element if not taked out of flow
@@ -50,22 +50,66 @@ export class MapViewComponent implements AfterViewInit {
       attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    this.http.get('./assets/geoJSON/malawi-admin-boundaries.json').subscribe((data: any) => {
-      const layerStyle = {
-        color: '#703aa3',
-        weight: 1,
-        opacity: 0.9,
-        fillOpacity: 0.1,
+    this.http.get('./assets/exampleData/sub-region-results_copy.json').subscribe((data: any) => {
+      // const layerStyle = {
+      //   color: '#703aa3',
+      //   weight: 1,
+      //   opacity: 0.9,
+      //   fillOpacity: 0.1,
+      // };
+
+      const style = (feature) => ({
+        fillColor: this.getColour(feature.properties.mn_absolute),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+      });
+
+      L.geoJSON(data, { style: style }).addTo(map);
+
+      const legend = L.control({ position: 'bottomright' });
+
+      legend.onAdd = () => {
+        const div = L.DomUtil.create('div', 'info legend');
+        const range = [0, 10, 50, 100, 250, 500, 1000, 1500];
+
+        // loop through our  intervals and generate a label with a colored square for each interval
+        for (let i = 0; i < range.length; i++) {
+          div.innerHTML +=
+            `<span style="display: flex; align-items: center;
+            ">
+            <span style="background-color:${this.getColour(
+              range[i] + 1,
+            )}; height:10px; width:10px; display:block; margin-right:5px;"> </span>` +
+            '<span>' +
+            range[i] +
+            (range[i + 1] ? `&ndash;${range[i + 1]}<br>` : '+') +
+            '</span>' +
+            '</span>';
+        }
+
+        return div;
       };
 
+      legend.addTo(map);
+
       const props = {
-        style: layerStyle,
+        // style: layerStyle,
         onEachFeature: (feature: any, layer: any) => {
           // console.log(feature, layer);
-          const layerName = feature.properties.NAME_1;
+          const layerName = feature.properties.subregion_name;
+          const layerValueAbsolute = feature.properties.mn_absolute;
+          const layerValueThreshold = feature.properties.mn_threshold;
+
           const layerCentre = layer.getBounds().getCenter();
 
-          const popup = L.popup().setLatLng(layerCentre).setContent(`<div>${layerName}</div>`);
+          const popup = L.popup()
+            .setLatLng(layerCentre)
+            .setContent(
+              `<div>Region:<b>${layerName}</b><br>Absolute value:${layerValueAbsolute}mg<br>Threshold: ${layerValueThreshold}%</div>`,
+            );
 
           layer.bindPopup(popup);
 
@@ -84,8 +128,25 @@ export class MapViewComponent implements AfterViewInit {
     return map;
   }
 
+  public getColour(absoluteValue: number): string {
+    return absoluteValue > 1500
+      ? '#2ca25f'
+      : absoluteValue > 1000
+      ? '#addd8e'
+      : absoluteValue > 500
+      ? '#ffeda0'
+      : absoluteValue > 250
+      ? '#feb24c'
+      : absoluteValue > 100
+      ? '#f03b20'
+      : absoluteValue > 50
+      ? '#bd0026'
+      : absoluteValue > 10
+      ? '#7a0177'
+      : '#354969';
+  }
+
   public openDialog(): void {
     void this.modalService.openMapDialog('Hello World');
   }
-
 }
