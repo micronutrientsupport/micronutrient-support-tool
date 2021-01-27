b0cfe7252e8b46f32b7e8df4067e3e0f52bdcd8f
jonstu <jonstu@bgs.ac.uk>
2021-01-27 10:12:00 +0000

    chore: try to fix lint of auto unsubscribe decorator

--- src/app/decorators/autoUnsubscribe.decorator.ts
+++ src/app/decorators/autoUnsubscribe.decorator.ts
@@ -1,23 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /**
  * Attempts to unsubscribe from any subscriptions that have been signed up to
  * when the ngOnDestroy method is called on a object with a lifecycle.
  */
-export function AutoUnsubscribe( blackList = [] ) {
+export function AutoUnsubscribe(blackList: Array<unknown> = []): (constructor: any) => void {
 
-  const unsubscriber = (property) => {
+  const unsubscriber = (property: Record<string, unknown>) => {
     if ((property != null) && (typeof property.unsubscribe === 'function')) {
       property.unsubscribe();
     }
   };
 
-  return (constructor) => {
-    const original = constructor.prototype.ngOnDestroy;
+  return (constructor: any) => {
+    const original: () => void = constructor.prototype.ngOnDestroy;
 
-    constructor.prototype.ngOnDestroy = function () {
+    constructor.prototype.ngOnDestroy = (...args: any) => {
       for (const prop of Object.keys(this)) {
-        const property = this[ prop ];
-        if ( blackList.indexOf(prop) === -1 ) {
-          if ( property ) {
+        const property = this[prop];
+        if (blackList.indexOf(prop) === -1) {
+          if (property) {
             if (Array.isArray(property)) {
               for (const element of property) {
                 unsubscriber(element);
@@ -29,7 +31,7 @@ export function AutoUnsubscribe( blackList = [] ) {
         }
       }
       if (original && typeof original === 'function') {
-        original.apply(this, arguments);
+        original.apply(this, args);
       }
     };
   };
