fcee6c27fd30f701113e28463910bfb5739d6bdc
jonstu <jonstu@bgs.ac.uk>
2021-01-28 16:40:47 +0000

    chore: remove unused component

--- src/app/components/dialogs/dialog.service.ts
+++ src/app/components/dialogs/dialog.service.ts
@@ -2,7 +2,6 @@ import { ComponentType } from '@angular/cdk/portal';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BaseDialogService, DialogData } from './baseDialogService.abstract';
-import { MapDialogComponent } from './mapDialog/mapDialog.component';
 import { ScenarioTypeDialogComponent } from './scenarioTypeDialog/scenarioTypeDialog.component';
 import { ShareDialogComponent } from './shareDialog/dialogShare.component';
 @Injectable({
@@ -13,12 +12,6 @@ export class DialogService extends BaseDialogService {
     super(dialog);
   }
 
-  // should be renamed to openMapDialog;
-  public openMapDialog(contentValue: string): Promise<DialogData> {
-    return this.openDialog('mapDialog', MapDialogComponent, true, {
-      content: contentValue,
-    });
-  }
   public openShareDialog(shareLink: string): Promise<DialogData> {
     return this.openDialog('sharingDialog', ShareDialogComponent, true, { shareLink });
   }
