0c4051d7bab6425cc083c9a96273a3a846f7f4a8
jonstu <jonstu@bgs.ac.uk>
2021-01-28 14:19:29 +0000

    refactor: update projection availability component to use new version of card and dialog

--- src/app/pages/quickMaps/pages/projection/projectionAvailability/projectionAvailability.component.ts
+++ src/app/pages/quickMaps/pages/projection/projectionAvailability/projectionAvailability.component.ts
@@ -1,43 +1,64 @@
 /* eslint-disable @typescript-eslint/dot-notation */
-import { ChangeDetectionStrategy, EventEmitter, Input, OnDestroy, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { PopulationGroup } from 'src/app/apiAndObjects/objects/populationGroup';
-import { Dictionary } from 'src/app/apiAndObjects/_lib_code/objects/dictionary';
-import { Subscription } from 'rxjs';
-import { GridsterItem } from 'angular-gridster2';
+import { ChangeDetectionStrategy, Input, OnInit, ChangeDetectorRef, Optional, Inject, Component } from '@angular/core';
+
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { Card2Component } from 'src/app/components/card2/card2.component';
+import { CurrentDataService } from 'src/app/services/currentData.service';
+import { QuickMapsService } from '../../../quickMaps.service';
+import { DialogService } from 'src/app/components/dialogs/dialog.service';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from 'src/app/components/dialogs/baseDialogService.abstract';
 @Component({
   selector: 'app-proj-avail',
   templateUrl: './projectionAvailability.component.html',
   styleUrls: ['./projectionAvailability.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectionAvailabilityComponent implements OnInit, OnDestroy {
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  @Input()
-  widget;
-  @Input()
-  resizeEvent: EventEmitter<GridsterItem>;
-  resizeSub: Subscription;
+export class ProjectionAvailabilityComponent implements OnInit {
+  @Input() card: Card2Component;
 
-  public countriesDictionary: Dictionary;
-  public regionDictionary: Dictionary;
-  public micronutrientsDictionary: Dictionary;
-  public popGroupOptions = new Array<PopulationGroup>();
-  public loading = false;
-  public error = false;
+  public title = 'Projection Availability';
 
-  constructor() {}
+  private loadingSrc = new BehaviorSubject<boolean>(false);
+  private errorSrc = new BehaviorSubject<boolean>(false);
+
+  private subscriptions = new Array<Subscription>();
+
+  constructor(
+    private currentDataService: CurrentDataService,
+    private quickMapsService: QuickMapsService,
+    private dialogService: DialogService,
+    private cdr: ChangeDetectorRef,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data?: DialogData) { }
 
   ngOnInit(): void {
-    this.resizeSub = this.resizeEvent.subscribe((widget) => {
-      if (widget === this.widget) {
-      }
+    // if displayed within a card component init interactions with the card
+    if (null != this.card) {
+      this.card.title = this.title;
+      this.card.showExpand = true;
+      this.card
+        .setLoadingObservable(this.loadingSrc.asObservable())
+        .setErrorObservable(this.errorSrc.asObservable());
+
+      this.subscriptions.push(
+        this.card.onExpandClickObs.subscribe(() => this.openDialog())
+      );
+    }
+
+    // respond to parameter updates
+    this.quickMapsService.parameterChangedObs.subscribe(() => {
+      this.loadingSrc.next(true);
+      // do stuff here
+
+      // TODO: remove when stuff added
+      setTimeout(() => {
+        this.loadingSrc.next(false);
+      }, 500);
     });
   }
 
-  ngOnDestroy(): void {
-    this.resizeSub.unsubscribe();
+  private openDialog(): void {
+    void this.dialogService.openDialogForComponent(ProjectionAvailabilityComponent);
   }
+
 }
