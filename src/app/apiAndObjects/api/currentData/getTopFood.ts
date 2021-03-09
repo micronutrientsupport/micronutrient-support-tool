8beb7a705b48d0588472f86dd0ffa888733db7b2
Warren <danwar@bgs.ac.uk>
2021-03-09 08:42:08 +0000

    refactor: updated topFoods page

--- src/app/apiAndObjects/api/currentData/getTopFood.ts
+++ src/app/apiAndObjects/api/currentData/getTopFood.ts
@@ -1,27 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { HttpClient } from '@angular/common/http';
 import { MicronutrientDictionaryItem } from '../../objects/dictionaries/micronutrientDictionaryItem';
 import { TopFoodSource } from '../../objects/topFoodSource';
 import { CacheableEndpoint } from '../../_lib_code/api/cacheableEndpoint.abstract';
+import { RequestMethod } from '../../_lib_code/api/requestMethod.enum';
 
 export class GetTopFood extends CacheableEndpoint<Array<TopFoodSource>, TopFoodParams, TopFoodSource> {
   protected getCacheKey(params: TopFoodParams): string {
     return JSON.stringify(params);
   }
   protected callLive(
-  // params: TopFoodParams,
+    params: TopFoodParams,
   ): Promise<Array<TopFoodSource>> {
-    throw new Error('Method not implemented.');
-    // const callResponsePromise = this.apiCaller.doCall('', RequestMethod.GET, {
-    //   'country-or-group-id': params.countryOrGroupId,
-    //   'micronutrient-id': params.micronutrientId,
-    //   'poulationGroup-id': params.poulationGroupId,
-    // });
+    // throw new Error('Method not implemented.');
+    const callResponsePromise = this.apiCaller.doCall([
+      'country',
+      'top20',
+      params.countryOrGroupId,
+      params.micronutrients[0].id,
+      // compositionId,
+      // consumptionId,
+    ], RequestMethod.GET).then((data: Array<Record<string, unknown>>) => this.processResponseData(data, params));
 
-    // return this.buildObjectsFromResponse(TopFoodSource, callResponsePromise);
+    return this.buildObjectsFromResponse(TopFoodSource, callResponsePromise);
   }
 
   protected callMock(
-  // params: TopFoodParams,
+    // params: TopFoodParams,
   ): Promise<Array<TopFoodSource>> {
     const httpClient = this.injector.get<HttpClient>(HttpClient);
     return this.buildObjectsFromResponse(
@@ -42,6 +50,15 @@ export class GetTopFood extends CacheableEndpoint<Array<TopFoodSource>, TopFoodP
         }, 1500);
       }),
     );
+  }
+
+  private processResponseData(
+    data: Array<Record<string, unknown>>,
+    params: TopFoodParams,
+  ): Array<Record<string, unknown>> {
+    data.forEach((item: Record<string, unknown>, index: number) => item.id = String(index).valueOf());
+    // return only first item when single option specified
+    return (params.countryOrGroupId, params.micronutrients[0].id) ? data.slice(0, 1) : data;
   }
 }
 
