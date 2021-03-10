b9c103c8f94506113754da7d644e80354aa7e8f8
Steve Richardson <stever@bgs.ac.uk>
2021-03-10 11:03:44 +0000

    chore: remove unused code

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -109,5 +109,3 @@ export const getRoute = (route: AppRoute, subsValues?: Array<string>): Array<str
   }
   return [url];
 };
-
-export const getSegments = (route: AppRoute): Array<string> => ['bing'];
