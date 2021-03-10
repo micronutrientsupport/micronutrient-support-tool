13b5abf663de299c8794a4d6d40d2ca400806a23
Warren <danwar@bgs.ac.uk>
2021-03-10 10:27:47 +0000

    style: made width fixed, not associated to width of screen

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -20,6 +20,6 @@ export class HeaderComponent {
     this.notificationService.sendInformative('Measure of MND changed to', 'Biomarker Data');
   }
   public testNegative(): void {
-    this.notificationService.sendNegative('An error occurred:', 'data could not be loaded');
+    this.notificationService.sendNegative('An error occurred:', 'data could not be loaded', 10000000);
   }
 }
