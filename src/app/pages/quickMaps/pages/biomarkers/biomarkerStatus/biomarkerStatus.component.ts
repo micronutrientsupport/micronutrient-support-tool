40c313b14c43d2e64e4574edd2acbc003f5c0be6
Yousaf <jyou@bgs.ac.uk>
2021-04-21 12:56:30 +0100

    feat: title in card

--- src/app/pages/quickMaps/pages/biomarkers/biomarkerStatus/biomarkerStatus.component.ts
+++ src/app/pages/quickMaps/pages/biomarkers/biomarkerStatus/biomarkerStatus.component.ts
@@ -1,38 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import * as L from 'leaflet';
+import { Component, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { ChartJSObject } from 'src/app/apiAndObjects/objects/misc/chartjsObject';
 import { MatTabGroup } from '@angular/material/tabs';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MonthlyFoodGroup } from 'src/app/apiAndObjects/objects/monthlyFoodGroup';
+import { CardComponent } from 'src/app/components/card/card.component';
 @Component({
   selector: 'app-biomarker-status',
   templateUrl: './biomarkerStatus.component.html',
-  styleUrls: ['./biomarkerStatus.component.scss']
+  styleUrls: ['../../expandableTabGroup.scss', './biomarkerStatus.component.scss']
 })
 export class BiomarkerStatusComponent implements AfterViewInit {
   @ViewChild(MatTabGroup) tabGroup: MatTabGroup;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('map1') map1Element: ElementRef;
+
+  @Input() card: CardComponent;
+
   public chartData: ChartJSObject;
   public dialogData: any;
-  public title = 'Biomarker Status';
+  public title = 'Zinc Status - Adult Women';
   public dataSource: MatTableDataSource<MonthlyFoodGroup>;
-
-  public displayedColumns = [
-    'a',
-    'b',
-    'c',
-    'd'
-  ];
-
-  public displayedColumns2 = [
-    'a',
-    'b',
-    'c'
-  ];
+  public displayedColumns = ['a', 'b', 'c', 'd'];
+  public displayedColumns2 = ['a', 'b', 'c'];
+  private biomarkerMap: L.Map;
 
   constructor() { }
   ngAfterViewInit(): void {
+    this.card.title = this.title;
+    this.card.showExpand = true;
+    this.biomarkerMap = this.initialiseMap(this.map1Element.nativeElement);
 
     this.chartData = {
       type: 'boxplot',
@@ -42,8 +41,8 @@ export class BiomarkerStatusComponent implements AfterViewInit {
         datasets: [
           {
             label: 'Dataset 1',
-            backgroundColor: () => 'rgba(255,0,0,0.5)',
-            borderColor: 'red',
+            backgroundColor: () => 'rgba(220,0,255,0.5)',
+            borderColor: 'rgba(220,0,255,0.5)',
             data: [
               this.randomValues(100, 0, 100),
               this.randomValues(100, 0, 20),
@@ -55,8 +54,8 @@ export class BiomarkerStatusComponent implements AfterViewInit {
           },
           {
             label: 'Dataset 2',
-            backgroundColor: () => 'rgba(0,0,255,0.5)',
-            borderColor: 'blue',
+            backgroundColor: () => 'rgba(0,220,255,0.5)',
+            borderColor: 'rgba(0,220,255,0.5)',
             data: [
               this.randomValues(100, 60, 100),
               this.randomValues(100, 0, 100),
@@ -73,7 +72,16 @@ export class BiomarkerStatusComponent implements AfterViewInit {
 
   }
 
-  randomValues(count: number, min: number, max: number): any {
+  private initialiseMap(mapElement: HTMLElement): L.Map {
+    const map = L.map(mapElement, {}).setView([6.6194073, 20.9367017], 3);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    return map;
+  }
+
+  private randomValues(count: number, min: number, max: number): any {
     const delta = max - min;
     return Array.from({ length: count }).map(() => Math.random() * delta + min);
   }
