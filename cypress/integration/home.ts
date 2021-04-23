9e04ca22929a09411b343be6d342c23c88e354bb
jonstu <jonstu@bgs.ac.uk>
2021-04-23 15:59:02 +0100

    test: try to get test to pass

--- cypress/integration/home.ts
+++ cypress/integration/home.ts
@@ -3,7 +3,7 @@
 describe('Homepage Tests', () => {
 
   it('loads the home page sharing dialog', () => {
-    cy.visit('/', { timeout: 10000 });
+    cy.visit('/', { timeout: 20000 });
     cy.get('#sharePageButton').click();
     cy.get('#shareContentPanel').should('be.visible');
     cy.get('#shareContentPanel > h2').contains('Share this page');
