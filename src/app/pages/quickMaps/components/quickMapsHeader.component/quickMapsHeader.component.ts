55c77417a1a3f7f9c058cd935316b28d267017d4
Steve Richardson <stever@bgs.ac.uk>
2021-04-27 16:13:33 +0100

    fix: prevent double titles being displayed with improved check of mnd

--- src/app/pages/quickMaps/components/quickMapsHeader.component/quickMapsHeader.component.ts
+++ src/app/pages/quickMaps/components/quickMapsHeader.component/quickMapsHeader.component.ts
@@ -4,6 +4,7 @@ import { AppRoutes } from 'src/app/routes/routes';
 import { Component } from '@angular/core';
 import { QuickMapsService } from '../../quickMaps.service';
 import { SharingService } from 'src/app/services/sharing.service';
+import { MicronutrientMeasureType } from 'src/app/apiAndObjects/objects/enums/micronutrientMeasureType.enum';
 @Component({
   selector: 'app-quick-maps-header',
   templateUrl: './quickMapsHeader.component.html',
@@ -19,5 +20,8 @@ export class QuickMapsHeaderComponent {
 
   public share(text: string, title?: string): Promise<any> {
     return this.sharingService.share(text, title, window.location.href);
+  }
+  public get MicronutrientMeasureType(): typeof MicronutrientMeasureType {
+    return MicronutrientMeasureType;
   }
 }
