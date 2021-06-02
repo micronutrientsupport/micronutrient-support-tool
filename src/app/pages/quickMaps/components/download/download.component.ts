efa604e0887d9de6347efbebb245359897eee283
Svea <svea@bgs.ac.uk>
2021-06-02 16:05:05 +0100

    refactor: csv download working with treemap data

--- src/app/pages/quickMaps/components/download/download.component.ts
+++ src/app/pages/quickMaps/components/download/download.component.ts
@@ -10,34 +10,37 @@ import { ExportService } from 'src/app/services/export.service';
 export class DownloadComponent implements OnInit {
   @Input() chartDownloadPNG: string;
   @Input() chartDownloadPDF: string;
-
-  users = [];
+  // users = [];
+  @Input() csvObject: Array<any>;
 
   constructor(private exportService: ExportService) {}
 
   ngOnInit(): void {
-    this.users = [
-      {
-        id: 1,
-        firstName: 'Mark',
-        lastName: 'Otto',
-        handle: '@mdo',
-      },
-      {
-        id: 2,
-        firstName: 'Jacob',
-        lastName: 'Thornton',
-        handle: '@fat',
-      },
-      {
-        id: 3,
-        firstName: 'Larry',
-        lastName: 'the Bird',
-        handle: '@twitter',
-      },
-    ];
+    // this.users = [
+    //   {
+    //     id: 1,
+    //     firstName: 'Mark',
+    //     lastName: 'Otto',
+    //     handle: '@mdo',
+    //   },
+    //   {
+    //     id: 2,
+    //     firstName: 'Jacob',
+    //     lastName: 'Thornton',
+    //     handle: '@fat',
+    //   },
+    //   {
+    //     id: 3,
+    //     firstName: 'Larry',
+    //     lastName: 'the Bird',
+    //     handle: '@twitter',
+    //   },
+    // ];
   }
+  // exportToCsv(): void {
+  //   this.exportService.exportToCsv(this.csvObject, 'user-data', ['id', 'firstName', 'lastName', 'handle']);
+  // }
   exportToCsv(): void {
-    this.exportService.exportToCsv(this.users, 'user-data', ['id', 'firstName', 'lastName', 'handle']);
+    this.exportService.exportToCsv(this.csvObject);
   }
 }
--- src/app/services/export.service.ts
+++ src/app/services/export.service.ts
@@ -4,6 +4,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Injectable } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 import * as FileSaver from 'file-saver';
 
 const CSV_EXTENSION = '.csv';
@@ -11,7 +12,7 @@ const CSV_TYPE = 'text/plain;charset=utf-8';
 
 @Injectable()
 export class ExportService {
-  constructor() {}
+  constructor(private parser: Papa) {}
   /**
    * Creates an array of data to csv. It will automatically generate title row based on object keys.
    *
@@ -21,36 +22,74 @@ export class ExportService {
    */
 
   // eslint-disable-next-line @typescript-eslint/ban-types
-  public exportToCsv(rows: object[], fileName: string, columns?: string[]): string {
-    if (!rows || !rows.length) {
-      return;
-    }
-    const separator = ',';
-    const keys = Object.keys(rows[0]).filter((k) => {
-      if (columns?.length) {
-        return columns.includes(k);
-      } else {
-        return true;
-      }
+  // public exportToCsv(rows: object[], fileName: string, columns?: string[]): string {
+  //   if (!rows || !rows.length) {
+  //     return;
+  //   }
+  //   const separator = ',';
+  //   const keys = Object.keys(rows[0]).filter((k) => {
+  //     if (columns?.length) {
+  //       return columns.includes(k);
+  //     } else {
+  //       return true;
+  //     }
+  //   });
+  //   const csvContent =
+  //     keys.join(separator) +
+  //     '\n' +
+  //     rows
+  //       .map((row) =>
+  //         keys
+  //           .map((k) => {
+  //             let cell = row[k] === null || row[k] === undefined ? '' : row[k];
+  //             cell = cell instanceof Date ? cell.toLocaleString() : cell.toString().replace(/"/g, '""');
+  //             if (cell.search(/("|,|\n)/g) >= 0) {
+  //               cell = `"${cell}"`;
+  //             }
+  //             return cell;
+  //           })
+  //           .join(separator),
+  //       )
+  //       .join('\n');
+  //   this.saveAsFile(csvContent, `${fileName}${CSV_EXTENSION}`, CSV_TYPE);
+  // }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  public exportToCsv(details: object[]): void {
+    // const exportObjectArray = new Array<ExportObject>();
+    // exportObjectArray.push({
+    //   clientSampleLabID: 'Lab Sample ID',
+    //   labComment: 'Lab Comment',
+    //   splitType: 'Split Type',
+    //   location: 'Location',
+    //   label: 'Label'
+    // });
+
+    // [].concat(...this.savedSamplesMap.values()).forEach((labSample: LabSample) => {
+    //   const exportObject = new ExportObject();
+    //   exportObject.clientSampleLabID = labSample.clientSampleLabsRef;
+    //   exportObject.labComment = labSample.description;
+    //   exportObject.splitType = labSample.splitType.name;
+    //   exportObject.location = labSample.location.name;
+    //   exportObject.label = labSample.label;
+    //   exportObjectArray.push(exportObject);
+    // });
+
+    const csvData = this.parser.unparse(details, {
+      header: true,
     });
-    const csvContent =
-      keys.join(separator) +
-      '\n' +
-      rows
-        .map((row) =>
-          keys
-            .map((k) => {
-              let cell = row[k] === null || row[k] === undefined ? '' : row[k];
-              cell = cell instanceof Date ? cell.toLocaleString() : cell.toString().replace(/"/g, '""');
-              if (cell.search(/("|,|\n)/g) >= 0) {
-                cell = `"${cell}"`;
-              }
-              return cell;
-            })
-            .join(separator),
-        )
-        .join('\n');
-    this.saveAsFile(csvContent, `${fileName}${CSV_EXTENSION}`, CSV_TYPE);
+
+    if (csvData != null) {
+      const a = document.createElement('a');
+      const blob = new Blob([csvData], { type: 'text/csv' });
+      const url = window.URL.createObjectURL(blob);
+
+      a.href = url;
+      a.download = 'test.csv';
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove();
+    }
   }
 
   /**
