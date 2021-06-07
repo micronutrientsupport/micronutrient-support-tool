cb1b778ddd2d41c46068c31c5b70ca1a23672b5e
Warren <danwar@bgs.ac.uk>
2021-06-07 10:23:01 +0100

    feat: create map component

--- src/app/pages/quickMaps/pages/dietaryChange/components/dietaryChangeComponents.module.ts
+++ src/app/pages/quickMaps/pages/dietaryChange/components/dietaryChangeComponents.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MapComponent } from './comparisonCard/map/map.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MapComponent
+  ],
   imports: [],
   providers: [],
 })
