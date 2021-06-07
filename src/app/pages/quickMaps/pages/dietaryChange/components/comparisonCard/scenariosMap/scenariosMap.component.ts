7ffec5ed4cc485a5e244f0d87ae786e648c5696c
Warren <danwar@bgs.ac.uk>
2021-06-07 13:41:50 +0100

    chore: working with svea

--- src/app/pages/quickMaps/pages/dietaryChange/components/comparisonCard/scenariosMap/scenariosMap.component.ts
+++ src/app/pages/quickMaps/pages/dietaryChange/components/comparisonCard/scenariosMap/scenariosMap.component.ts
@@ -1,5 +1,6 @@
 import { ElementRef } from '@angular/core';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { LeafletMapHelper } from 'src/app/other/leafletMapHelper';
 
 @Component({
   selector: 'app-scenarios-map',
@@ -9,7 +10,23 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class ScenariosMapComponent implements OnInit {
   @ViewChild('map1') map1Element: ElementRef;
   @ViewChild('map2') map2Element: ElementRef;
+
+  public baselineMap: L.Map;
+  public scenarioMap: L.Map;
+  private areaBounds: L.LatLngBounds;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.baselineMap = this.initialiseMap(this.map1Element.nativeElement);
+    this.scenarioMap = this.initialiseMap(this.map2Element.nativeElement);
+  }
+
+  private initialiseMap(mapElement: HTMLElement): L.Map {
+    return new LeafletMapHelper()
+      .createMap(mapElement)
+      .setDefaultBaseLayer()
+      .setDefaultControls(() => this.areaBounds)
+      .getMap();
+  }
 }
