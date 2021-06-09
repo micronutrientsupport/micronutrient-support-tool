6e07fd9425e1018abc7799416c12aa704268b35a
Warren <danwar@bgs.ac.uk>
2021-06-09 11:58:09 +0100

    feat: added colour palette details

--- src/app/pages/quickMaps/pages/dietaryChange/components/comparisonCard/scenariosMap/scenariosMap.component.ts
+++ src/app/pages/quickMaps/pages/dietaryChange/components/comparisonCard/scenariosMap/scenariosMap.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, ElementRef, Input } from '@angular/core';
 import { Component, ViewChild } from '@angular/core';
 import { SubRegionDataItem } from 'src/app/apiAndObjects/objects/subRegionDataItem';
 import { LeafletMapHelper } from 'src/app/other/leafletMapHelper';
+import { ColourPalette } from 'src/app/pages/quickMaps/components/colourObjects/colourPalette';
+import { ColourPaletteType } from 'src/app/pages/quickMaps/components/colourObjects/colourPaletteType.enum';
 
 @Component({
   selector: 'app-scenarios-map',
@@ -9,6 +11,7 @@ import { LeafletMapHelper } from 'src/app/other/leafletMapHelper';
   styleUrls: ['./scenariosMap.component.scss'],
 })
 export class ScenariosMapComponent implements AfterViewInit {
+  public static readonly COLOUR_PALETTE_ID = 'scenarios-map-view';
   @ViewChild('baselineMap') baselineMapElement: ElementRef;
   @ViewChild('scenarioMap') scenarioMapElement: ElementRef;
 
@@ -18,15 +21,25 @@ export class ScenariosMapComponent implements AfterViewInit {
 
   public baselineMapData: SubRegionDataItem;
 
-  public baselineMap: L.Map;
-  public scenarioMap: L.Map;
-
-  public allowBaselineMapEvents: boolean;
-  public allowScenarioMapEvents: boolean;
-
+  private baselineMap: L.Map;
+  private scenarioMap: L.Map;
   private areaBounds: L.LatLngBounds;
 
-  constructor() {}
+  private allowBaselineMapEvents: boolean;
+  private allowScenarioMapEvents: boolean;
+
+  private defaultPalette = ColourPalette.PALETTES.find(
+    (value: ColourPalette) => value.name === ColourPaletteType.BLUEREDYELLOWGREEN,
+  );
+  private colourPalette: ColourPalette;
+
+  constructor() {
+    this.colourPalette = ColourPalette.getSelectedPalette(ScenariosMapComponent.COLOUR_PALETTE_ID);
+    if (null == this.colourPalette) {
+      ColourPalette.setSelectedPalette(ScenariosMapComponent.COLOUR_PALETTE_ID, this.defaultPalette);
+      this.colourPalette = this.defaultPalette;
+    }
+  }
 
   ngAfterViewInit(): void {
     this.baselineMap = this.initialiseBaselineMap(this.baselineMapElement.nativeElement);
