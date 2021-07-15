d4a0330d14a3834439a6b491e215133cab1b7b92
Warren <danwar@bgs.ac.uk>
2021-07-15 16:49:33 +0100

    chore: removed dead code

--- src/app/pages/quickMaps/pages/biomarkers/biomarkerStatus/biomarkerStatus.component.ts
+++ src/app/pages/quickMaps/pages/biomarkers/biomarkerStatus/biomarkerStatus.component.ts
@@ -13,15 +13,12 @@ import {
   Inject,
   ChangeDetectorRef,
 } from '@angular/core';
-import { ChartJSObject } from 'src/app/apiAndObjects/objects/misc/chartjsObject';
-import * as ChartAnnotation from 'chartjs-plugin-annotation';
 import { MatTabChangeEvent, MatTabGroup } from '@angular/material/tabs';
 import { CardComponent } from 'src/app/components/card/card.component';
 import { FormControl } from '@angular/forms';
 import { ChartjsComponent } from '@ctrl/ngx-chartjs';
 import { QuickMapsService } from 'src/app/pages/quickMaps/quickMaps.service';
 import { CurrentDataService } from 'src/app/services/currentData.service';
-import { QuickchartService } from 'src/app/services/quickChart.service';
 import { DialogService } from 'src/app/components/dialogs/dialog.service';
 import { DialogData } from 'src/app/components/dialogs/baseDialogService.abstract';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -68,12 +65,9 @@ export class BiomarkerStatusComponent implements AfterViewInit {
   @ViewChild('settingsMenu', { static: true }) menu: MatMenu;
 
   public chartTypeValue;
-  public boxChartData: ChartJSObject;
-  public barChartData: ChartJSObject;
   public title: string;
   public selectedTab: number;
-  public defThreshold = 20;
-  public abnThreshold = 60;
+
   public showOutliers = true;
   public outlierControl = new FormControl(true);
   public dataTypes = new FormControl();
@@ -131,7 +125,6 @@ export class BiomarkerStatusComponent implements AfterViewInit {
     private http: HttpClient,
     private papa: Papa,
     private currentDataService: CurrentDataService,
-    private qcService: QuickchartService,
     private dialogService: DialogService,
     private cdr: ChangeDetectorRef,
     private biomarkerService: BiomarkerService,
@@ -144,7 +137,6 @@ export class BiomarkerStatusComponent implements AfterViewInit {
     }
   }
   ngAfterViewInit(): void {
-    // this.absoluteMap = this.initialiseMap(this.mapElement.nativeElement);
     this.card.showExpand = true;
     this.card.setLoadingObservable(this.loadingSrc.asObservable()).setErrorObservable(this.errorSrc.asObservable());
     this.subscriptions.push(this.card.onExpandClickObs.subscribe(() => this.openDialog()));
@@ -162,20 +154,10 @@ export class BiomarkerStatusComponent implements AfterViewInit {
       }),
     );
 
-    // Render all charts initially for download;
-    this.renderAllCharts();
     this.card.showExpand = true;
     this.card.showSettingsMenu = true;
     this.card.matMenu = this.menu;
     this.card.setLoadingObservable(this.loadingSrc.asObservable()).setErrorObservable(this.errorSrc.asObservable());
-
-    this.initialiseBoxChart([
-      this.randomBoxPlot(0, 100),
-      this.randomBoxPlot(0, 20),
-      this.randomBoxPlot(20, 70),
-      this.randomBoxPlot(60, 100),
-      this.randomBoxPlot(50, 100),
-    ]);
 
     this.subscriptions.push(
       this.quickMapsService.parameterChangedObs.subscribe(() => {
@@ -195,152 +177,6 @@ export class BiomarkerStatusComponent implements AfterViewInit {
   public navigateToInfoTab(): void {
     this.selectedTab = 4;
     this.cdr.detectChanges();
-  }
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  public initialiseBoxChart(data: any): void {
-    this.boxChartData = {
-      plugins: [ChartAnnotation],
-      type: 'boxplot',
-      data: {
-        labels: ['Central', 'North', 'South', 'South East', 'West'],
-        datasets: [
-          {
-            label: this.quickMapsService.micronutrient.name,
-            backgroundColor: 'rgba(0,220,255,0.5)',
-            borderColor: 'rgba(0,220,255,0.5)',
-            outlierColor: 'rgba(0,0,0,0.2)',
-            outlierRadius: 3,
-            data: data,
-          },
-        ],
-      },
-      options: {
-        maintainAspectRatio: true,
-        title: {
-          display: true,
-          text: 'Boxplot',
-        },
-        annotation: {
-          annotations: [
-            {
-              type: 'line',
-              id: 'defLine',
-              mode: 'horizontal',
-              scaleID: 'y-axis-0',
-              value: this.defThreshold,
-              borderWidth: 2.0,
-              borderColor: 'rgba(255,0,0,0.5)',
-              label: {
-                enabled: true,
-                content: 'Deficiency threshold',
-                backgroundColor: 'rgba(255,0,0,0.8)',
-              },
-            },
-            {
-              type: 'line',
-              id: 'abnLine',
-              mode: 'horizontal',
-              scaleID: 'y-axis-0',
-              value: this.abnThreshold,
-              borderWidth: 2.0,
-              borderColor: 'rgba(0,0,255,0.5)',
-              label: {
-                enabled: true,
-                content: 'Threshold for abnormal values',
-                backgroundColor: 'rgba(0,0,255,0.8)',
-              },
-            },
-          ],
-        },
-      },
-    };
-
-    const chartForRender: ChartJSObject = JSON.parse(JSON.stringify(this.boxChartData));
-    this.boxChartPNG = this.qcService.getChartAsImageUrl(chartForRender, 'png');
-    this.boxChartPDF = this.qcService.getChartAsImageUrl(chartForRender, 'pdf');
-  }
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  public initialiseBarChart(dataObj: any, type: string): void {
-    let title = '';
-    switch (type) {
-      case 'pod':
-        title = `Prevalence of ${this.selectedNutrient} deficiency`;
-        break;
-      case 'poe':
-        title = `Prevalence of ${this.selectedNutrient} excess`;
-        break;
-      case 'cde':
-        title = `Combined prevalence of ${this.selectedNutrient} deficiency and excess`;
-        break;
-    }
-    title = title + ' per participants characteristics';
-
-    this.barChartData = {
-      plugins: [ChartAnnotation],
-      type: 'bar',
-      data: dataObj,
-      options: {
-        title: {
-          display: true,
-          text: title,
-        },
-        maintainAspectRatio: true,
-        legend: {
-          display: true,
-          position: 'bottom',
-          align: 'center',
-        },
-        scales: {
-          xAxes: [
-            {
-              display: true,
-            },
-          ],
-          yAxes: [
-            {
-              display: true,
-              id: 'y-axis-0',
-            },
-          ],
-        },
-      },
-    };
-
-    const chartForRender: ChartJSObject = JSON.parse(JSON.stringify(this.barChartData));
-    switch (type) {
-      case 'pod':
-        this.deficiencyBarChartPNG = this.qcService.getChartAsImageUrl(chartForRender, 'png');
-        this.deficiencyBarChartPDF = this.qcService.getChartAsImageUrl(chartForRender, 'pdf');
-        break;
-      case 'poe':
-        this.excessBarChartPNG = this.qcService.getChartAsImageUrl(chartForRender, 'png');
-        this.excessBarChartPDF = this.qcService.getChartAsImageUrl(chartForRender, 'pdf');
-        break;
-      case 'cde':
-        this.combinedBarChartPNG = this.qcService.getChartAsImageUrl(chartForRender, 'png');
-        this.combinedBarChartPDF = this.qcService.getChartAsImageUrl(chartForRender, 'pdf');
-        break;
-    }
-  }
-
-  public toggleShowOutlier(): void {
-    this.showOutliers = this.outlierControl.value;
-    if (!this.showOutliers) {
-      // Hide outliers
-      this.boxChartData.data.datasets[0].data.forEach((data: any) => {
-        this.outlierSet.push(data.outliers);
-        data.outliers = null;
-      });
-      this.boxPlot.renderChart();
-    } else {
-      // Show outliers
-      this.boxChartData.data.datasets[0].data.forEach((data: any, idx: number) => {
-        data.outliers = this.outlierSet[idx];
-      });
-      this.boxPlot.renderChart();
-    }
   }
 
   public tabChanged(tabChangeEvent: MatTabChangeEvent): void {
@@ -369,8 +205,6 @@ export class BiomarkerStatusComponent implements AfterViewInit {
         break;
       case 'chart':
         this.chartTypeValue = value;
-        const barData = this.getBarData(value);
-        this.initialiseBarChart(barData, this.selectedOption);
         break;
     }
   }
@@ -401,8 +235,6 @@ export class BiomarkerStatusComponent implements AfterViewInit {
         const blob = this.papa.parse(data, { header: true }).data;
         const dataArray = new Array<BiomarkerStatusData>();
 
-        // console.log(this.quickMapsService.ageGenderGroup.id); // all; adult_women; adult_men; children
-
         blob.forEach((simpleData) => {
           const statusData: BiomarkerStatusData = {
             areaName: simpleData.AreaName,
@@ -421,198 +253,6 @@ export class BiomarkerStatusComponent implements AfterViewInit {
       });
   }
 
-  private openDialog(): void {
-    void this.dialogService.openDialogForComponent<BiomarkerStatusDialogData>(BiomarkerStatusComponent, {
-      data: null,
-      selectedTab: this.tabGroup.selectedIndex,
-    });
-  }
-
-  private renderAllCharts() {
-    this.initialiseBoxChart([
-      this.randomBoxPlot(0, 100),
-      this.randomBoxPlot(0, 20),
-      this.randomBoxPlot(20, 70),
-      this.randomBoxPlot(60, 100),
-      this.randomBoxPlot(50, 100),
-    ]);
-
-    this.initialiseBarChart(this.getBarData('pod'), 'pod');
-    this.initialiseBarChart(this.getBarData('poe'), 'poe');
-    this.initialiseBarChart(this.getBarData('cde'), 'cde');
-  }
-
-  private getBarData(type: string): any {
-    const barLabel = [
-      'Central',
-      'North',
-      'South',
-      'South East',
-      'West',
-      '15-19',
-      '20-29',
-      '30-39',
-      'Urban',
-      'Rural',
-      'Poorest',
-      'Poorer',
-      'Middle',
-      'Richer',
-      'Richest',
-      'Total',
-    ];
-    switch (type) {
-      case 'pod':
-        return {
-          labels: ['Region', 'Age group', 'Residence', 'Wealth quintiles'],
-          datasets: [
-            {
-              label: barLabel[0],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[1],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[2],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[3],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[4],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-          ],
-        };
-
-      case 'poe':
-        return {
-          labels: ['Region', 'Age group', 'Residence', 'Wealth quintiles'],
-          datasets: [
-            {
-              label: barLabel[0],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#50AF5D',
-              backgroundColor: '#50AF5D',
-              fill: true,
-            },
-            {
-              label: barLabel[1],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#50AF5D',
-              backgroundColor: '#50AF5D',
-              fill: true,
-            },
-            {
-              label: barLabel[2],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#50AF5D',
-              backgroundColor: '#50AF5D',
-              fill: true,
-            },
-            {
-              label: barLabel[3],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#50AF5D',
-              backgroundColor: '#50AF5D',
-              fill: true,
-            },
-            {
-              label: barLabel[4],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#50AF5D',
-              backgroundColor: '#50AF5D',
-              fill: true,
-            },
-          ],
-        };
-
-      case 'cde':
-        return {
-          labels: ['Region', 'Age group', 'Residence', 'Wealth quintiles'],
-          datasets: [
-            {
-              label: barLabel[0],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[1],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[2],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[3],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-            {
-              label: barLabel[4],
-              data: this.randomValues(4, 0, 100),
-              borderColor: '#AF50A2',
-              backgroundColor: '#AF50A2',
-              fill: true,
-            },
-          ],
-        };
-      default:
-        return null;
-    }
-  }
-
-  private randomValues(count: number, min: number, max: number) {
-    const delta = max - min;
-    return Array.from({ length: count }).map(() => Math.random() * delta + min);
-  }
-
-  // Return the correct interface to dislpay outliers
-  private randomBoxPlot(min: any, max: any) {
-    const values = this.randomValues(5, min, max).sort((a, b) => a - b);
-
-    return {
-      min: values[0],
-      q1: values[1],
-      median: values[2],
-      q3: values[3],
-      max: values[4],
-      outliers: Array(20)
-        .fill(1)
-        .map(() => Math.round(Math.random() * 120)),
-    };
-  }
-
   private initMapData(dataPromise: Promise<SubRegionDataItem>): void {
     this.loadingSrc.next(true);
     dataPromise
@@ -628,5 +268,12 @@ export class BiomarkerStatusComponent implements AfterViewInit {
         this.loadingSrc.next(false);
         this.cdr.detectChanges();
       });
+  }
+
+  private openDialog(): void {
+    void this.dialogService.openDialogForComponent<BiomarkerStatusDialogData>(BiomarkerStatusComponent, {
+      data: null,
+      selectedTab: this.tabGroup.selectedIndex,
+    });
   }
 }
