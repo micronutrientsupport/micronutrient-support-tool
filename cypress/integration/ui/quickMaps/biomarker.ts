e0f19654966a9cd0430014b26bb7ad15d1eadb68
Steve Richardson <stever@bgs.ac.uk>
2021-09-09 08:27:59 +0100

    feat: added biomarker page with fixes to a11y checks

---
+++ cypress/integration/ui/quickMaps/biomarker.ts
@@ -0,0 +1,25 @@
+/// <reference path="../../../support/index.d.ts" />
+
+describe('Quick maps - Dietary Change', () => {
+  it('checks page for a11y compliance', () => {
+    cy.visit(
+      '/quick-maps/biomarker?country-id=MWI&mnd-id=I&measure=biomarker&data-level=household&age-gender-group=all',
+    );
+    cy.wait(3000);
+    cy.get('.minimize-button').click();
+    cy.wait(1000);
+    cy.injectAxe();
+    cy.checkA11y(
+      {
+        /*
+        @angular-material-extensions/fab-menu does not contain aria-label tags
+        so it fails the a11y tests. Until the directive can be updated we need to
+        ignore this element
+        */
+        exclude: ['.mat-fab'],
+      },
+      null,
+      cy.terminalLog,
+    );
+  });
+});
