3db76f3098e77e04a17b46beff57517810ea61ca
Steve Richardson <stever@bgs.ac.uk>
2021-09-09 09:15:18 +0100

    chore: linting fix

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,3 +1,4 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
-module.exports = (on, config) => {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+module.exports = (on, config) => {};
