ae9157fa234a6dfa15b63462fd0ac300aa1f594a
Steve Richardson <stever@bgs.ac.uk>
2021-09-10 14:47:25 +0100

    test: output sidebar change to compare to netlify preview version

--- src/app/pages/quickMaps/pages/projection/projectionAvailability/projectionAvailability.component.ts
+++ src/app/pages/quickMaps/pages/projection/projectionAvailability/projectionAvailability.component.ts
@@ -125,6 +125,7 @@ export class ProjectionAvailabilityComponent implements AfterViewInit {
   }
 
   private init(dataPromise: Promise<Array<ProjectedAvailability>>): void {
+    console.log('this.micronutrientId = ', this.micronutrientId);
     this.loadingSrc.next(true);
 
     void this.projectionDataService
