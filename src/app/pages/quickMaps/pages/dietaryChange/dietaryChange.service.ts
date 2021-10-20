41fb9a0a6dc2258c4b9b195577f8aa054dd23c22
jstu <jonstu@bgs.ac.uk>
2021-10-20 16:17:57 +0100

    chore: add the scenario mode to the query string and use it on load

--- src/app/pages/quickMaps/pages/dietaryChange/dietaryChange.service.ts
+++ src/app/pages/quickMaps/pages/dietaryChange/dietaryChange.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { DietaryChangeItem } from 'src/app/apiAndObjects/objects/dietaryChange.item';
+import { QuickMapsQueryParams } from '../../quickMapsQueryParams';
 import { DietaryChangeMode } from './dietaryChangeMode.enum';
 
 @Injectable()
@@ -27,14 +28,13 @@ export class DietaryChangeService {
 
   private parameterChangeTimeout: NodeJS.Timeout;
 
-  // private readonly quickMapsParameters: QuickMapsQueryParams;
+  private readonly quickMapsParameters: QuickMapsQueryParams;
 
-  constructor() {
-    // this.quickMapsParameters = new QuickMapsQueryParams(injector);
+  constructor(injector: Injector) {
+    this.quickMapsParameters = new QuickMapsQueryParams(injector);
 
     // set from query params etc. on init
-    // this.setMeasure(this.quickMapsParameters.getMeasure());
-    // this.setDataLevel(this.quickMapsParameters.getDataLevel());
+    this.setMode(this.quickMapsParameters.getScenarioMode());
 
     this.initSubscriptions();
     this.initSrc.next(true);
@@ -44,7 +44,9 @@ export class DietaryChangeService {
     return this.modeSrc.value;
   }
   public setMode(mode: DietaryChangeMode, force = false): void {
-    this.setValue(this.modeSrc, mode, force);
+    if (null != mode) {
+      this.setValue(this.modeSrc, mode, force);
+    }
   }
   public get changeItems(): Array<DietaryChangeItem> {
     return this.changeItemsSrc.value;
@@ -53,17 +55,11 @@ export class DietaryChangeService {
     this.setValue(this.changeItemsSrc, changeItems, force);
   }
 
-  // public updateQueryParams(): void {
-  //   const paramsObj = {} as Record<string, string | Array<string>>;
-  //   paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.COUNTRY_ID] = null != this.country ? this.country.id : null;
-  //   paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.MICRONUTRIENT_ID] =
-  //     null != this.micronutrient ? this.micronutrient.id : null;
-  //   paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.MEASURE] = this.measure;
-  //   paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.DATA_LEVEL] = this.dataLevel;
-  //   paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.AGE_GENDER_GROUP] =
-  //     null != this.ageGenderGroup ? this.ageGenderGroup.id : null;
-  //   this.quickMapsParameters.setQueryParams(paramsObj);
-  // }
+  public updateQueryParams(): void {
+    const paramsObj = {} as Record<string, string | Array<string>>;
+    paramsObj[QuickMapsQueryParams.QUERY_PARAM_KEYS.SCENARIO_MODE] = null == this.mode ? null : String(this.mode);
+    this.quickMapsParameters.setQueryParams(paramsObj);
+  }
 
   protected setValue<T>(srcRef: BehaviorSubject<T>, value: T, force: boolean): void {
     if (force || srcRef.value !== value) {
@@ -78,10 +74,10 @@ export class DietaryChangeService {
   }
 
   private parameterChanged(): void {
-    // this.updateQueryParams();
     // ensure not triggered too many times in quick succession
     clearTimeout(this.parameterChangeTimeout);
     this.parameterChangeTimeout = setTimeout(() => {
+      this.updateQueryParams();
       this.parameterChangedSrc.next();
     }, 100);
   }
