3c6f96635380244b3d657e01fcae019daee72b9e
jstu <jonstu@bgs.ac.uk>
2021-10-20 15:12:38 +0100

    chore: add comments to _lib api code (copied from apiHelper project)

--- src/app/apiAndObjects/_lib_code/api/cacheableEndpoint.abstract.ts
+++ src/app/apiAndObjects/_lib_code/api/cacheableEndpoint.abstract.ts
@@ -1,5 +1,8 @@
 import { Endpoint } from './endpoint.abstract';
 
+/**
+ * Extends Endpoint and adds caching functionality
+ */
 export abstract class CacheableEndpoint<RETURN_TYPE, PARAMS_TYPE, OBJECT_TYPE = RETURN_TYPE> extends Endpoint<
   RETURN_TYPE,
   PARAMS_TYPE,
@@ -10,6 +13,15 @@ export abstract class CacheableEndpoint<RETURN_TYPE, PARAMS_TYPE, OBJECT_TYPE = 
   protected cachedValues = new Map<string, RETURN_TYPE>();
   protected currentCallPromise: Promise<RETURN_TYPE>;
 
+  /**
+   * Looks for a previously cached value or a current ongoing call that can be returned as a promise.
+   * If none found, accessing Endpoint Call method and caches the result using the key returned from
+   * the [getCacheKey]{@link #getCacheKey} method.
+   *
+   * @param params params used for call and cache key generation (merged with and overrides defaults)
+   * @param useCache Overrides whether to use cache or not (defaults to [useCacheByDefault]{@link #useCacheByDefault})
+   * @returns Promise of call data
+   */
   public call(params?: PARAMS_TYPE, useCache?: boolean): Promise<RETURN_TYPE> {
     const cacheKey = this.getCacheKey(this.validateAndMergeParams(params));
     const cacheValue = this.getCacheValue(cacheKey);
@@ -35,28 +47,43 @@ export abstract class CacheableEndpoint<RETURN_TYPE, PARAMS_TYPE, OBJECT_TYPE = 
 
   /**
    * Sets whether to use caching.  Can be overridden when using [call]{@link #call} function.
+   *
+   * @param useCache Changes default caching on/off
+   * @returns This instance
    */
   protected setUseCacheByDefault(useCache: boolean): this {
     this.useCacheByDefault = useCache;
     return this;
   }
 
+  /**
+   * @param key identifier for the data required
+   * @returns cached data from a previous callout if exists
+   */
   protected getCacheValue(key: string): RETURN_TYPE {
     return this.cachedValues.get(key);
   }
-
+  /**
+   * Sets a cache value (normally after a successful callout)
+   *
+   * @param key identifier for the data required
+   * @param value data value to store
+   */
   protected setCacheValue(key: string, value: RETURN_TYPE): void {
     this.cachedValues.set(key, value);
   }
 
+  /**
+   * clears the current cached data
+   */
   protected clearCache(): void {
     this.cachedValues.clear();
   }
 
   /**
-   * Retrieves the key to identify a cached response within this object.
+   * Retrieves the key to identify a cached data within this object.
    * Usually returns an id, a concatonation of other params
-   * or any static string if there is only ever one response.
+   * or any static string if there is only ever one data.
    */
   protected abstract getCacheKey(params: PARAMS_TYPE): string;
 }
--- src/app/apiAndObjects/_lib_code/api/getDictionary.ts
+++ src/app/apiAndObjects/_lib_code/api/getDictionary.ts
@@ -8,57 +8,84 @@ import { RequestMethod } from './requestMethod.enum';
 type OBJECT_TYPE = DictionaryItem;
 type RETURN_TYPE = Dictionary;
 
-export class GetDictionary
-  extends CacheableEndpoint<RETURN_TYPE, GetDictionaryItemsParams, OBJECT_TYPE> {
-
+/**
+ * The Endpoint implementation for a dictionary endpoint
+ */
+export class GetDictionary extends CacheableEndpoint<RETURN_TYPE, GetDictionaryItemsParams, OBJECT_TYPE> {
   public mockObjectsCreatorFunc: (injector: Injector) => Promise<Array<Record<string, unknown>>>;
   protected mockItemsCount = 20;
   protected mockObjects: Array<Record<string, unknown>>;
 
-
+  /**
+   * @param type An identifier for the dictionary (normally an enum value)
+   * @param isLive Whether to use callLive or callMock method
+   */
   constructor(public readonly type: unknown, isLive?: boolean) {
     super(isLive);
   }
 
+  /**
+   * @param count Number of mock items to make
+   * @returns A reference to this object
+   */
   public setMockItemGenerationCount(count: number): this {
     this.mockItemsCount = count;
     return this;
   }
   /**
    * Set mock objects as they would be returned from a faked API call
+   *
+   * @param objects Objects to return from a mock api call
+   * @returns A reference to this object
    */
   public setMockObjects(objects: Array<Record<string, unknown>>): this {
     this.mockObjects = objects;
     return this;
   }
 
+  /**
+   * @param func A function that will create mock objects.  This function gets the Injector passed in,
+   * so if you get an HttpClient from there you can call out for mock data files in your assets, for instance.
+   * @returns A reference to this object
+   */
   public setMockObjectsCreatorFunc(func: (injector: Injector) => Promise<Array<Record<string, unknown>>>): this {
     this.mockObjectsCreatorFunc = func;
     return this;
   }
 
+  /**
+   * @param params params object
+   * @returns The key that will reference this response in the cache.
+   */
   protected getCacheKey(params: GetDictionaryItemsParams): string {
     return params.path;
   }
-
+  /**
+   * Calls out for live api dictionary data
+   *
+   * @param params parameters required to make the call
+   * @returns A Promise of the raw data
+   */
   protected callLive(params: GetDictionaryItemsParams): Promise<RETURN_TYPE> {
     const callResponsePromise = this.apiCaller.doCall(params.path, RequestMethod.GET);
 
     return this.buildObjectsFromResponse(params.typeObj, callResponsePromise).then(
-      (items: Array<OBJECT_TYPE>) =>
-        new Dictionary(this.type, (items as unknown) as Array<OBJECT_TYPE>),
+      (items: Array<OBJECT_TYPE>) => new Dictionary(this.type, items as unknown as Array<OBJECT_TYPE>),
     );
   }
 
+  /**
+   * @param params parameters required to make the call
+   * @returns Mock data items set using setMockObjects or produced using the function
+   * passed to setMockObjectsCreatorFunc.
+   */
   protected callMock(params: GetDictionaryItemsParams): Promise<RETURN_TYPE> {
     return this.buildObjectsFromResponse(
       params.typeObj,
-      ((null == this.mockObjectsCreatorFunc)
-        ? Promise.resolve((null != this.mockObjects) ? this.mockObjects.slice() : [])
-        : this.mockObjectsCreatorFunc(this.injector))
-    ).then(
-      (items: Array<OBJECT_TYPE>) => new Dictionary(this.type, (items as unknown) as Array<OBJECT_TYPE>),
-    );
+      null == this.mockObjectsCreatorFunc
+        ? Promise.resolve(null != this.mockObjects ? this.mockObjects.slice() : [])
+        : this.mockObjectsCreatorFunc(this.injector),
+    ).then((items: Array<OBJECT_TYPE>) => new Dictionary(this.type, items as unknown as Array<OBJECT_TYPE>));
   }
 }
 
