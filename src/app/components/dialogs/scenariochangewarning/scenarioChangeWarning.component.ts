c348e953c3c627a86e43508c2566584315210f84
Daniel Warren - BGS <danwar@bgs.ac.uk>
2021-10-21 11:45:39 +0100

    feat: created new info dialog

---
+++ src/app/components/dialogs/scenariochangewarning/scenarioChangeWarning.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scenario-change-warning',
+  templateUrl: './scenarioChangeWarning.component.html',
+  styleUrls: ['./scenarioChangeWarning.component.scss'],
+})
+export class ScenarioChangeWarningComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
