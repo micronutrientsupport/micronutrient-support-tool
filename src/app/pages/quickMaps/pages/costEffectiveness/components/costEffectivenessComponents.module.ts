5630a122bb0d4e1eed0e7954744165bca809bb88
Daniel Warren - BGS <danwar@bgs.ac.uk>
2021-11-25 16:17:03 +0000

    feat: created menu component and added components

--- src/app/pages/quickMaps/pages/costEffectiveness/components/costEffectivenessComponents.module.ts
+++ src/app/pages/quickMaps/pages/costEffectiveness/components/costEffectivenessComponents.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DescriptionComponent } from './description/description.component';
 import { AppMaterialModule } from 'src/app/app-material.module';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [DescriptionComponent],
+  declarations: [DescriptionComponent, MenuComponent],
   imports: [CommonModule, AppMaterialModule],
-  exports: [DescriptionComponent],
+  exports: [DescriptionComponent, MenuComponent],
 })
 export class CostEffectivenessComponentsModule {}
