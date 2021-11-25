17b7cf5b008a9a02acb8694b9057d3d5303fd3db
Daniel Warren - BGS <danwar@bgs.ac.uk>
2021-11-25 16:42:46 +0000

    feat: added two further components

--- src/app/pages/quickMaps/pages/costEffectiveness/components/costEffectivenessComponents.module.ts
+++ src/app/pages/quickMaps/pages/costEffectiveness/components/costEffectivenessComponents.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DescriptionComponent } from './description/description.component';
 import { AppMaterialModule } from 'src/app/app-material.module';
 import { MenuComponent } from './menu/menu.component';
+import { InterventionCreationComponent } from './interventionCreation/interventionCreation.component';
+import { InterventionComparisonComponent } from './interventionComparison/interventionComparison.component';
 
 @NgModule({
-  declarations: [DescriptionComponent, MenuComponent],
+  declarations: [DescriptionComponent, MenuComponent, InterventionCreationComponent, InterventionComparisonComponent],
   imports: [CommonModule, AppMaterialModule],
-  exports: [DescriptionComponent, MenuComponent],
+  exports: [DescriptionComponent, MenuComponent, InterventionCreationComponent, InterventionComparisonComponent],
 })
 export class CostEffectivenessComponentsModule {}
---
+++ src/app/pages/quickMaps/pages/costEffectiveness/components/interventionCreation/interventionCreation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intervention-creation',
+  templateUrl: './interventionCreation.component.html',
+  styleUrls: ['./interventionCreation.component.scss'],
+})
+export class InterventionCreationComponent implements OnInit {
+  constructor() {
+    // add content
+  }
+
+  ngOnInit(): void {
+    // add content
+  }
+}
