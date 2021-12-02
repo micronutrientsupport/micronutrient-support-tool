42f682f691cc52077bd7102d26e33825f92745f5
Steve Richardson <stever@bgs.ac.uk>
2021-12-02 15:27:18 +0000

    ci: update compodoc

--- cypress/integration/ui/quickMaps/locationSelect.ts
+++ cypress/integration/ui/quickMaps/locationSelect.ts
@@ -7,17 +7,18 @@ describe('Quick Map - Location Select', () => {
   //   cy.injectAxe();
   //   cy.checkA11y(null, null, cy.terminalLog);
   // });
-  // it('accesses leafet map', () => {
-  //   cy.visit('/quick-maps');
-  //   cy.wait(5000);
-  //   cy.get('.leaflet-container').should('be.visible');
-  //   cy.get('.leaflet-container > .leaflet-map-pane').should('exist');
-  //   cy.window()
-  //     // set leaflet map instance reference
-  //     .then((win: unknown) => {
-  //       leafletObject = win['testing'].leafletObject;
-  //       // check map object available
-  //       assert.isNotNull(leafletObject);
-  //     });
-  // });
+
+  it('accesses leafet map', () => {
+    cy.visit('/quick-maps');
+    cy.wait(5000);
+    cy.get('.leaflet-container').should('be.visible');
+    cy.get('.leaflet-container > .leaflet-map-pane').should('exist');
+    cy.window()
+      // set leaflet map instance reference
+      .then((win: unknown) => {
+        leafletObject = win['testing'].leafletObject;
+        // check map object available
+        assert.isNotNull(leafletObject);
+      });
+  });
 });
