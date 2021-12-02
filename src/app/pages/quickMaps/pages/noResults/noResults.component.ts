8458d4adae0886e4b0a46be5ba5e3f85adeca2ea
Steve Richardson <stever@bgs.ac.uk>
2021-12-02 14:14:16 +0000

    feat: improve no data placeholder content and add more info to no side bar results

--- src/app/pages/quickMaps/pages/noResults/noResults.component.ts
+++ src/app/pages/quickMaps/pages/noResults/noResults.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { QuickMapsService } from '../../quickMaps.service';
 
 @Component({
   selector: 'app-quickmaps-no-results',
   templateUrl: './noResults.component.html',
   styleUrls: ['./noResults.component.scss'],
 })
-export class NoResultsComponent {}
+export class NoResultsComponent {
+  constructor(public quickMapsService: QuickMapsService) {}
+}
