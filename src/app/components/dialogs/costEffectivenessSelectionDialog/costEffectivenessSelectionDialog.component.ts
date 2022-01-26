1406aff8d7bcf5171caca56f96e8e8c6fe918d54
Daniel Warren - BGS <danwar@bgs.ac.uk>
2022-01-26 10:51:56 +0000

    feat: intervention can be selected in intervention selection component

--- src/app/components/dialogs/costEffectivenessSelectionDialog/costEffectivenessSelectionDialog.component.ts
+++ src/app/components/dialogs/costEffectivenessSelectionDialog/costEffectivenessSelectionDialog.component.ts
@@ -2,12 +2,6 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { InterventionsDictionaryItem } from 'src/app/apiAndObjects/objects/dictionaries/interventionDictionaryItem';
 import { DialogData } from '../baseDialogService.abstract';
-
-interface NameValue {
-  name?: string;
-  value?: string;
-}
-
 @Component({
   selector: 'app-costEffectivenessSelectionDialog',
   templateUrl: './costEffectivenessSelectionDialog.component.html',
@@ -15,12 +9,7 @@ interface NameValue {
 })
 export class CostEffectivenessSelectionDialogComponent implements OnInit {
   public interventions: Array<InterventionsDictionaryItem>;
-  public readonly interventionArray: NameValue[] = [
-    { name: 'Wheat fortification', value: 'Lorem Ipsum Paragraph1' },
-    { name: 'Supplementation', value: 'Lorem Ipsum Paragraph2' },
-    { name: 'Salt fortification', value: 'Lorem Ipsum Paragraph3' },
-  ];
-  public interventionNameValue: NameValue = {};
+  public selectedIntervention: InterventionsDictionaryItem;
   constructor(public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public dialogData: DialogData) {
     this.interventions = dialogData.dataIn;
   }
@@ -30,6 +19,13 @@ export class CostEffectivenessSelectionDialogComponent implements OnInit {
   }
 
   public closeDialog(): void {
+    this.dialog.closeAll();
+  }
+
+  public createIntervention(): void {
+    if (null != this.selectedIntervention) {
+      this.dialogData.dataOut = this.selectedIntervention;
+    }
     this.dialog.closeAll();
   }
 }
