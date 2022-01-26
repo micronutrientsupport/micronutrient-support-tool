71a52d69991f7a8ce4e2121052b5eff30ff54a35
Daniel Warren - BGS <danwar@bgs.ac.uk>
2022-01-26 15:32:04 +0000

    feat: created intervention object and coded live intervention call

---
+++ src/app/apiAndObjects/api/intervention/getIntervention.ts
@@ -0,0 +1,42 @@
+/* tslint:disable: no-string-literal */
+import { Intervention } from '../../objects/intervention';
+import { CacheableEndpoint } from '../../_lib_code/api/cacheableEndpoint.abstract';
+import { RequestMethod } from '../../_lib_code/api/requestMethod.enum';
+
+export class GetIntervention extends CacheableEndpoint<Array<Intervention>, GetInverventionsParams, Intervention> {
+  protected getCacheKey(params: GetInverventionsParams): string {
+    return JSON.stringify(params);
+  }
+
+  protected callLive(params: GetInverventionsParams): Promise<Array<Intervention>> {
+    const callResponsePromise = this.apiCaller.doCall(['interventions', params.id], RequestMethod.GET, {
+      id: params.id,
+    });
+
+    return this.buildObjectsFromResponse(Intervention, callResponsePromise);
+  }
+
+  protected callMock(): Promise<Array<Intervention>> {
+    // const httpClient = this.injector.get<HttpClient>(HttpClient);
+    // return this.buildObjectsFromResponse(
+    //   MonthlyFoodGroup,
+    //   // response after delay
+    //   new Promise((resolve) => {
+    //     setTimeout(() => {
+    //       resolve(httpClient.get('/assets/exampleData/monthly-food-groups.json').toPromise());
+    //     }, 1500);
+    //   }).then((data: Record<string, unknown>) => {
+    //     if (null != data) {
+    //       // change something so that the display changes a little
+    //       // eslint-disable-next-line @typescript-eslint/dot-notation
+    //       data[0][MonthlyFoodGroup.KEYS.PERCENTAGE_MN_CONSUMED] = Math.floor(Math.random() * 40);
+    //     }
+    return null;
+    //   }),
+    // );
+  }
+}
+
+export interface GetInverventionsParams {
+  id: string;
+}
