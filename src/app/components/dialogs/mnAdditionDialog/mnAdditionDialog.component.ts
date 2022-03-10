50672f77acce75923c15cad538c3681fd1a2d726
Svea Rautenberg - BGS <svea@bgs.ac.uk>
2022-03-10 11:02:55 +0000

    chore: retrieving baseline assumption data

--- src/app/components/dialogs/mnAdditionDialog/mnAdditionDialog.component.ts
+++ src/app/components/dialogs/mnAdditionDialog/mnAdditionDialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '../baseDialogService.abstract';
 
 @Component({
   selector: 'app-mn-addition-dialog',
@@ -6,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mnAdditionDialog.component.scss'],
 })
 export class MnAdditionDialogComponent implements OnInit {
-  constructor() {
+  constructor(public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public dialogData: DialogData) {
     //add content
   }
 
   ngOnInit(): void {
     //add content
+  }
+  public closeDialog(): void {
+    this.dialog.closeAll();
+  }
+
+  public addMN(): void {
+    //add additional micronutrient
+    //
+    this.dialog.closeAll();
   }
 }
--- src/app/pages/quickMaps/pages/costEffectiveness/interventionReview/pages/interventionBaseline/interventionBaseline.component.ts
+++ src/app/pages/quickMaps/pages/costEffectiveness/interventionReview/pages/interventionBaseline/interventionBaseline.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { InterventionBaselineAssumptions } from 'src/app/apiAndObjects/objects/interventionBaselineAssumptions';
+import {
+  BaselineAssumptions,
+  InterventionBaselineAssumptions,
+} from 'src/app/apiAndObjects/objects/interventionBaselineAssumptions';
 import { DialogService } from 'src/app/components/dialogs/dialog.service';
 import { AppRoutes } from 'src/app/routes/routes';
+import { InterventionDataService } from 'src/app/services/interventionData.service';
 import { InterventionSideNavContentService } from '../../components/interventionSideNavContent/interventionSideNavContent.service';
 
 @Component({
@@ -11,13 +15,36 @@ import { InterventionSideNavContentService } from '../../components/intervention
   styleUrls: ['./interventionBaseline.component.scss'],
 })
 export class InterventionBaselineComponent {
-  constructor(private dialogService: DialogService, private intSideNavService: InterventionSideNavContentService) {}
+  constructor(
+    private interventionDataService: InterventionDataService,
+    private dialogService: DialogService,
+    private intSideNavService: InterventionSideNavContentService,
+  ) {}
   public dataSource = new MatTableDataSource();
   public ROUTES = AppRoutes;
   public pageStepperPosition = 0;
 
   public ngOnInit(): void {
     this.intSideNavService.setCurrentStepperPosition(this.pageStepperPosition);
+
+    void this.interventionDataService
+      .getInterventionBaselineAssumptions('1')
+      .then((data: InterventionBaselineAssumptions) => {
+        this.createTableObject(data);
+      });
+  }
+  public createTableObject(data: InterventionBaselineAssumptions): void {
+    const dataArray = [];
+
+    const rawData = data.baselineAssumptions as BaselineAssumptions;
+
+    dataArray.push(rawData.actuallyFortified, rawData.potentiallyFortified);
+
+    console.debug(rawData);
+
+    this.dataSource = new MatTableDataSource(dataArray);
+
+    // this.createAvNutrientLevelTable(rawData);
   }
 
   public resetValues(): void {
