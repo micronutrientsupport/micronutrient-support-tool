7dd95af04699c249ba556d5028f54ee809e44c7c
Svea Rautenberg - BGS <svea@bgs.ac.uk>
2022-03-15 16:35:08 +0000

    feat: added reusable intervention review footer component under utilities, merged Dan S graph changes

---
+++ src/app/pages/quickMaps/pages/costEffectiveness/interventionReview/utilities/reusableInterventionReviewFooter/reusableInterventionReviewFooter.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { DialogService } from 'src/app/components/dialogs/dialog.service';
+
+@Component({
+  selector: 'app-reusable-intervention-review-footer',
+  templateUrl: './reusableInterventionReviewFooter.component.html',
+  styleUrls: ['./reusableInterventionReviewFooter.component.scss'],
+})
+export class InterventionReviewFooterComponent implements OnInit {
+  constructor(private dialogService: DialogService) {}
+
+  ngOnInit(): void {
+    //add content
+  }
+
+  public resetValues(): void {
+    void this.dialogService.openCEResetDialog();
+  }
+}
