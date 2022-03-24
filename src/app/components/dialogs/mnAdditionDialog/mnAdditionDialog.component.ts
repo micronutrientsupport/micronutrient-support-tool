6558935e7e49a7b67fdb8c55a49da6c3e3af5308
Daniel Warren - BGS <danwar@bgs.ac.uk>
2022-03-24 14:35:33 +0000

    feat: can display list of mns

--- src/app/components/dialogs/mnAdditionDialog/mnAdditionDialog.component.ts
+++ src/app/components/dialogs/mnAdditionDialog/mnAdditionDialog.component.ts
@@ -1,7 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { throwToolbarMixedModesError } from '@angular/material/toolbar';
 import { DictionaryType } from 'src/app/apiAndObjects/api/dictionaryType.enum';
+import { MicronutrientDictionaryItem } from 'src/app/apiAndObjects/objects/dictionaries/micronutrientDictionaryItem';
 import { Dictionary } from 'src/app/apiAndObjects/_lib_code/objects/dictionary';
+import { DictionaryItem } from 'src/app/apiAndObjects/_lib_code/objects/dictionaryItem.interface';
 import { DictionaryService } from 'src/app/services/dictionary.service';
 import { DialogData } from '../baseDialogService.abstract';
 
@@ -12,6 +15,7 @@ import { DialogData } from '../baseDialogService.abstract';
 })
 export class MnAdditionDialogComponent implements OnInit {
   public micronutrientsDictionary: Dictionary;
+  public selectMNDsFiltered = new Array<DictionaryItem>();
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public dialogData: DialogData,
@@ -19,6 +23,7 @@ export class MnAdditionDialogComponent implements OnInit {
   ) {
     void dictionariesService.getDictionaries([DictionaryType.MICRONUTRIENTS]).then((dicts: Array<Dictionary>) => {
       this.micronutrientsDictionary = dicts.shift();
+      this.filterSortMn();
       console.debug(this.micronutrientsDictionary);
     });
   }
@@ -35,5 +40,9 @@ export class MnAdditionDialogComponent implements OnInit {
     //add additional micronutrient
     //
     this.dialogData.close();
+  }
+
+  public filterSortMn(): void {
+    this.selectMNDsFiltered = this.micronutrientsDictionary.getItems().sort((a, b) => (a.name < b.name ? -1 : 1));
   }
 }
