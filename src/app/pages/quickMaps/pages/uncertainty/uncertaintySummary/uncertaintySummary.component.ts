47249e1e1e37e148f7b995ed4e774ccd4421b846
Daniel Warren <danwar@bgs.ac.uk>
2022-06-28 11:43:54 +0100

    feat: fleshed scaffold and key some key values for summary card

--- src/app/pages/quickMaps/pages/uncertainty/uncertaintySummary/uncertaintySummary.component.ts
+++ src/app/pages/quickMaps/pages/uncertainty/uncertaintySummary/uncertaintySummary.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DictionaryType } from 'src/app/apiAndObjects/api/dictionaryType.enum';
+import { ImpactScenarioDictionaryItem } from 'src/app/apiAndObjects/objects/dictionaries/impactScenarioDictionaryItem';
+import { ProjectionsSummary } from 'src/app/apiAndObjects/objects/projectionSummary';
+import { DictionaryService } from 'src/app/services/dictionary.service';
+import { ProjectionDataService } from 'src/app/services/projectionData.service';
+import { QuickMapsService } from '../../../quickMaps.service';
 
 @Component({
-  selector: 'lib-uncertaintySummary',
+  selector: 'app-uncertainty-summary',
   templateUrl: './uncertaintySummary.component.html',
   styleUrls: ['./uncertaintySummary.component.scss'],
 })
 export class UncertaintySummaryComponent {
-  // constructor() { }
+  public loading: boolean;
+  public projectionsSummary: ProjectionsSummary;
+  private baselineScenario: ImpactScenarioDictionaryItem;
+  private subscriptions = new Array<Subscription>();
+
+  constructor(
+    public quickMapsService: QuickMapsService,
+    private readonly cdr: ChangeDetectorRef,
+    private readonly dictionaryService: DictionaryService,
+    private readonly projectionDataService: ProjectionDataService,
+  ) {
+    void this.dictionaryService.getDictionaries([DictionaryType.IMPACT_SCENARIOS]).then((dicts) => {
+      this.baselineScenario = dicts
+        .shift()
+        .getItems<ImpactScenarioDictionaryItem>()
+        .find((item) => item.isBaseline);
+
+      this.subscriptions.push(
+        this.quickMapsService.parameterChangedObs.subscribe(() => {
+          this.updateProjectionSummary();
+        }),
+      );
+    });
+  }
+
+  private updateProjectionSummary(): void {
+    this.loading = true;
+    void this.projectionDataService
+      .getProjectionSummaries(
+        this.quickMapsService.country.get(),
+        this.quickMapsService.micronutrient.get(),
+        this.baselineScenario,
+      )
+      .catch(() => null)
+      .then((summary: ProjectionsSummary) => {
+        this.projectionsSummary = summary;
+      })
+      .finally(() => {
+        // this.calculate();
+        this.cdr.markForCheck();
+        this.loading = false;
+      });
+  }
   // ngOnInit() {
   // }
 }
