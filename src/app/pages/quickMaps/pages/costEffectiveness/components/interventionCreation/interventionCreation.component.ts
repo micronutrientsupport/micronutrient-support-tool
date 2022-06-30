3105f19e491ab7de6981e8299c7917cb7d58414f
Daniel Sutton - BGS <dsut@bgs.ac.uk>
2022-06-30 15:46:14 +0100

    feat: url stores selected intervention ids and loads them on page load

--- src/app/pages/quickMaps/pages/costEffectiveness/components/interventionCreation/interventionCreation.component.ts
+++ src/app/pages/quickMaps/pages/costEffectiveness/components/interventionCreation/interventionCreation.component.ts
@@ -6,6 +6,9 @@ import { DialogData } from 'src/app/components/dialogs/baseDialogService.abstrac
 import { DialogService } from 'src/app/components/dialogs/dialog.service';
 import { QuickMapsService } from 'src/app/pages/quickMaps/quickMaps.service';
 import { DictionaryService } from 'src/app/services/dictionary.service';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { InterventionDataService } from 'src/app/services/interventionData.service';
 
 @Component({
   selector: 'app-intervention-creation',
@@ -16,20 +19,39 @@ import { DictionaryService } from 'src/app/services/dictionary.service';
 export class InterventionCreationComponent {
   public interventionsDictionaryItems: Array<InterventionsDictionaryItem>;
   public selectedInterventions: Array<InterventionsDictionaryItem> = [];
+  public routerSubscription: Subscription;
 
   constructor(
     public quickMapsService: QuickMapsService,
     private dialogService: DialogService,
     private dictionariesService: DictionaryService,
+    private interventionService: InterventionDataService,
     private cdr: ChangeDetectorRef,
+    private location: Location,
   ) {
     void dictionariesService.getDictionaries([DictionaryType.INTERVENTIONS]).then((dicts: Array<Dictionary>) => {
       this.interventionsDictionaryItems = dicts.shift().getItems();
     });
+
+    const searchParams = new URLSearchParams(window.location.search);
+    const inteventionIds = searchParams.getAll('intervention_ids');
+    if (inteventionIds) {
+      inteventionIds.forEach(id => {
+        this.interventionService.getIntervention(id).then((data: any) => {
+          this.selectedInterventions.push(data);
+          this.cdr.detectChanges();
+        });
+      });
+    }
   }
+
 
   public openCESelectionDialog(): void {
     void this.dialogService.openCESelectionDialog(this.interventionsDictionaryItems).then((data: DialogData) => {
+      const searchParams = new URLSearchParams(window.location.search);
+      searchParams.append('intervention_ids', data.dataOut.id)
+      this.location.replaceState(`${location.pathname}?${searchParams}`);
+
       this.selectedInterventions.push(data.dataOut);
       this.cdr.detectChanges();
     });
