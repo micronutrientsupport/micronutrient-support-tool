67fdedbbc8fa8b085a6bdb51a075c4716f653698
Steve Richardson <stever@bgs.ac.uk>
2023-02-08 11:25:13 +0000

    test: tidy

--- cypress/integration/ui/home.ts
+++ cypress/integration/ui/home.ts
@@ -10,29 +10,4 @@ describe('Page: Home Tests', () => {
     cy.injectAxe();
     cy.checkA11y(null, null, cy.terminalLog);
   });
-
-  it('loads the home page sharing dialog', () => {
-    cy.visit('/');
-    cy.get('#sharePageButton').click();
-    cy.get('#shareContentPanel').should('be.visible');
-    cy.get('#shareContentPanel > h2').contains('Share this page');
-  });
-
-  it('loads the home page sharing dialog and tries to close by pressing esc key', () => {
-    cy.visit('/');
-    cy.get('#sharePageButton').click();
-    cy.get('#sharingDialog').should('be.visible');
-    cy.get('#shareContentPanel > h2').contains('Share this page');
-    cy.get('body').type('{esc}', { force: true });
-    cy.get('#sharingDialog').should('not.exist');
-  });
-
-  it('loads the home page sharing dialog and tries to close by clicking in surrounding page', () => {
-    cy.visit('/');
-    cy.get('#sharePageButton').click();
-    cy.get('#sharingDialog').should('be.visible');
-    cy.get('#shareContentPanel > h2').contains('Share this page');
-    cy.get('.cdk-overlay-backdrop').click(-50, -50, { force: true });
-    cy.get('#sharingDialog').should('not.exist');
-  });
 });
