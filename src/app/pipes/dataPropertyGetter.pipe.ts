ae6aff5f30c5e6fcbe6bdd518e49ab210bb0609f
Steve Richardson <stever@bgs.ac.uk>
2023-02-16 12:02:34 +0000

    chore: fix linting

--- src/app/pipes/dataPropertyGetter.pipe.ts
+++ src/app/pipes/dataPropertyGetter.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dataPropertyGetter',
 })
 export class DataPropertyGetterPipe implements PipeTransform {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
   transform(object: any, keyName: string, ...args: unknown[]): unknown {
     return object[keyName];
   }
