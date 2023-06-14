05eb7a799827b19934d05c65c8fb99578ad6bf3b
Daniel Warren <danwar@bgs.ac.uk>
2023-06-14 13:42:40 +0100

    feat: created mnTableCell component

---
+++ src/app/pages/quickMaps/pages/costEffectiveness/interventionReview/utilities/microNutrientsInPremixTable/premixTableRow/mnTableCell/mnTableCell.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mn-table-cell',
+  templateUrl: './mnTableCell.component.html',
+  styleUrls: ['./mnTableCell.component.scss'],
+})
+export class MnTableCellComponent {}
