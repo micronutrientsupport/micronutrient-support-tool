18c16587c2a513d75c44d51a3d35a723aba9d584
Daniel Warren <danwar@bgs.ac.uk>
2023-07-11 16:59:34 +0100

    feat: user can now log in and out

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,8 @@ import { AppRoutes } from 'src/app/routes/routes';
 import { DialogService } from '../dialogs/dialog.service';
 import { UntypedFormGroup } from '@angular/forms';
 import { ApiService } from 'src/app/apiAndObjects/api/api.service';
+import { LoginRegisterResponseDataSource } from 'src/app/apiAndObjects/objects/loginRegisterResponseDataSource';
+import { UserLoginService } from 'src/app/services/userLogin.service';
 
 @Component({
   selector: 'app-header',
@@ -12,16 +14,30 @@ import { ApiService } from 'src/app/apiAndObjects/api/api.service';
 export class HeaderComponent {
   public ROUTES = AppRoutes;
   public form!: UntypedFormGroup;
+  public activeUser: LoginRegisterResponseDataSource | null;
 
-  constructor(private dialogService: DialogService, private apiService: ApiService) {}
+  constructor(
+    private dialogService: DialogService,
+    private apiService: ApiService,
+    private userLoginSevice: UserLoginService,
+  ) {
+    this.userLoginSevice.activeUserObs.subscribe((activeUser: LoginRegisterResponseDataSource | null) => {
+      this.activeUser = activeUser;
+      console.debug('active user', activeUser);
+    });
+  }
 
   public handleLoginDialog(): void {
     this.dialogService.openLoginDialog();
   }
 
+  public handleLogout(): void {
+    this.userLoginSevice.setActiveUser(null);
+  }
+
   public handleGetProfile() {
-    this.apiService.endpoints.user.getProfile.call().then((response) => {
-      console.debug(response);
-    });
+    // this.apiService.endpoints.user.getProfile.call().then((response) => {
+    //   console.debug(response);
+    // });
   }
 }
