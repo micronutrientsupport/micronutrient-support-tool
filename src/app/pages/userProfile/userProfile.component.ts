a9f1eae1187fb2498d2c07bbde132eb3faac0451
Daniel Warren <danwar@bgs.ac.uk>
2023-07-12 13:37:22 +0100

    feat: created content for profile page

--- src/app/pages/userProfile/userProfile.component.ts
+++ src/app/pages/userProfile/userProfile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/apiAndObjects/api/api.service';
+import { UserProfileDetailDataSource } from 'src/app/apiAndObjects/objects/userProfileDetailDataSource';
 import { UserLoginService } from 'src/app/services/userLogin.service';
 
 @Component({
@@ -8,9 +9,12 @@ import { UserLoginService } from 'src/app/services/userLogin.service';
   styleUrls: ['./userProfile.component.scss'],
 })
 export class UserProfileComponent {
+  public userProfile: UserProfileDetailDataSource;
   constructor(private userLoginService: UserLoginService, private apiService: ApiService) {
-    this.apiService.endpoints.user.getProfile.call(this.userLoginService.getActiveUser()).then((response) => {
-      console.debug(response);
-    });
+    this.apiService.endpoints.user.getProfile
+      .call(this.userLoginService.getActiveUser())
+      .then((response: UserProfileDetailDataSource) => {
+        this.userProfile = response;
+      });
   }
 }
