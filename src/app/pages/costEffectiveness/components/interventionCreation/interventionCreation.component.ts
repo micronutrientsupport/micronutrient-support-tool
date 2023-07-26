561e89b674b7aab938dba3f8c4b12b6f0fe6f83f
Daniel Warren <danwar@bgs.ac.uk>
2023-07-26 12:37:40 +0100

    feat: anonymous interventions now appear when logged out

--- src/app/pages/costEffectiveness/components/interventionCreation/interventionCreation.component.ts
+++ src/app/pages/costEffectiveness/components/interventionCreation/interventionCreation.component.ts
@@ -46,6 +46,8 @@ export class InterventionCreationComponent {
           return (
             intervention.userId === this.userLoginService.getActiveUser().id || intervention.userId === 'Anonymous'
           );
+        } else {
+          return intervention.userId === 'Anonymous';
         }
       });
       this.cdr.markForCheck();
