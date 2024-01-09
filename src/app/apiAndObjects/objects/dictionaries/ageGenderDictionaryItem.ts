6b365e27c2014c6f0ba642741770bca5a741af13
Steve Richardson <stever@bgs.ac.uk>
2024-01-09 09:25:28 +0000

    fix: calling biomarker data sources on agegender change and passing correct age group

--- src/app/apiAndObjects/objects/dictionaries/ageGenderDictionaryItem.ts
+++ src/app/apiAndObjects/objects/dictionaries/ageGenderDictionaryItem.ts
@@ -7,18 +7,29 @@ export class AgeGenderDictionaryItem extends MapsDictionaryItem {
   public static readonly ID_ATTRIBUTE: string = 'groupId';
   public static readonly NAME_ATTRIBUTE: string = 'groupName';
   public static readonly DESC_ATTRIBUTE: string = 'groupName';
+
+  public static readonly GROUP_ID: string = 'groupId';
+  public static readonly SUPRA_GROUP: string = 'supraGroup';
+  public static readonly GROUP_NAME: string = 'groupName';
+  public static readonly IS_DEFAULT: string = 'isDefault';
   public static readonly KEYS = {
-    GROUP: 'supraGroup',
+    GROUP_ID: 'groupId',
+    SUPRA_GROUP: 'supraGroup',
+    GROUP_NAME: 'groupName',
     IS_DEFAULT: 'isDefault',
   };
 
-  public readonly group: string;
+  public readonly groupId: string;
+  public readonly supraGroup: string;
+  public readonly groupName: string;
   public readonly isDefault: boolean;
 
   protected constructor(sourceObject: Record<string, unknown>, id: string, name: string, description: string) {
     super(sourceObject, id, name, description);
 
-    this.group = this._getString(AgeGenderDictionaryItem.KEYS.GROUP);
+    this.groupId = this._getString(AgeGenderDictionaryItem.KEYS.GROUP_ID);
+    this.supraGroup = this._getString(AgeGenderDictionaryItem.KEYS.SUPRA_GROUP);
+    this.groupName = this._getString(AgeGenderDictionaryItem.KEYS.GROUP_NAME);
     this.isDefault = this._getBoolean(AgeGenderDictionaryItem.KEYS.IS_DEFAULT);
   }
 
