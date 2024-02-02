7ebbbea7630c9163d1d3abf0403ed4965926335a
Bean, Andrew K <andan@bsg.ac.uk>
2024-02-02 10:42:35 +0000

    fix: fix switching between food systems and biomarker

--- src/app/components/noResults/noResults.component.ts
+++ src/app/components/noResults/noResults.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { QuickMapsService } from '../../pages/quickMaps/quickMaps.service';
+import { MicronutrientMeasureType } from 'src/app/apiAndObjects/objects/enums/micronutrientMeasureType.enum';
 
 @Component({
   selector: 'app-quickmaps-no-results',
@@ -7,5 +8,7 @@ import { QuickMapsService } from '../../pages/quickMaps/quickMaps.service';
   styleUrls: ['./noResults.component.scss'],
 })
 export class NoResultsComponent {
+  public MeasureType = MicronutrientMeasureType;
+
   constructor(public quickMapsService: QuickMapsService) {}
 }
