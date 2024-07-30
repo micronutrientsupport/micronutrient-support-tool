86d25025935bc6072f89cf13fb799bfb44e0bcb2
Bean, Andrew K <andan@bsg.ac.uk>
2024-07-30 20:24:05 +0100

    feat: add bm units to threhsold details.  Update placeholder text

--- src/app/apiAndObjects/objects/biomarker/biomarkerThresholds.ts
+++ src/app/apiAndObjects/objects/biomarker/biomarkerThresholds.ts
@@ -3,6 +3,7 @@ import { BaseObject } from '../../_lib_code/objects/baseObject';
 export type BiomarkerThreshold = {
   upper: number;
   lower: number;
+  unit: number;
   thresholdType: string;
   condition: string;
   comments: string;
